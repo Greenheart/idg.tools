9e57af25235924e00171b2d31dba90aeacc1c09f
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-01-25 21:06:15 +0100

    details

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,50 +1,27 @@
 <script lang="ts" context="module">
-    import { dev } from "$app/env"
 </script>
 
-<!-- IDEA: Allow players to adjust AI speed between 200 ms to 2000 ms per step -->
-
-<header class="shadow-lg bg-stone-800">
-    <div class="max-w-4xl w-full flex justify-between items-center mx-auto p-4">
-        <div class="flex items-center space-x-2 text-white">
+<header class="bg-stone-800 shadow-lg">
+    <div class="mx-auto flex w-full max-w-4xl items-center justify-between p-4">
+        <a href="https://matrix.to/#/#idg:matrix.org" target="_blank">
             <svg
+                class="h-6 w-6"
+                fill="#fff"
+                viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6 cursor-pointer"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
             >
                 <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"
+                    d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"
                 />
             </svg>
-            {#if dev}
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-6 w-6 cursor-pointer"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                >
-                    <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
-                        d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
-                    />
-                </svg>
-            {/if}
-        </div>
+        </a>
 
-        <h1 class="font-semibold text-4xl -mt-2">Regeneration</h1>
-        <a href="https://github.com/Greenheart/iso-conquest" target="_blank">
+        <h1 class="-mt-2 text-4xl font-semibold">Regeneration</h1>
+        <a href="https://github.com/Greenheart/regeneration" target="_blank">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 16 16"
-                class="w-6 h-6"
+                class="h-6 w-6"
                 fill="#fff"
             >
                 <path
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,5 @@
 <script lang="ts">
+    import Header from '$components/Header.svelte'
 </script>
+
+<Header />
