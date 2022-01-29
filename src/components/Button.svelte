d572f10120f6c0bb962d7480d8298ebc20d6e443
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-01-29 14:08:27 +0100

    Update deps and button
    Co-authored-by: chrismact <chrismact@users.noreply.github.com>

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -4,6 +4,7 @@
         secondary: 'bg-stone-400 text-black',
         danger: 'bg-red-700 text-white',
     }
+    const defaultVariant = 'primary'
 
     export type ButtonProps = {
         onClick: () => void
@@ -14,17 +15,17 @@
 </script>
 
 <script lang="ts">
-    export let variant: ButtonProps['variant']
+    export let variant: ButtonProps['variant'] = defaultVariant
     export let onClick: ButtonProps['onClick']
     export let label: ButtonProps['label']
-    export let autofocus: ButtonProps['autofocus']
+    export let autofocus: ButtonProps['autofocus'] = false
 </script>
 
 <!-- svelte-ignore a11y-autofocus -->
 <button
     {autofocus}
     class={'px-12 py-4 transform-gpu hover:scale-105 duration-100 text-xl font-semibold ' +
-        variants[variant ?? 'primary']}
+        variants[variant ?? defaultVariant]}
     on:click={onClick}
 >
     {label}
