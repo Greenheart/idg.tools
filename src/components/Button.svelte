678d03f7feed896f4f15ac8e8614f8c9997d7a66
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-01-29 19:01:29 +0100

    Improve button disabled state

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -5,9 +5,11 @@
         primary: 'bg-stone-50 text-stone-900',
         secondary: 'bg-transparent text-stone-50 border-stone-50',
         danger: 'bg-red-700 text-stone-50',
-        inactive: 'bg-stone-50 text-stone-900 opacity-60',
+        disabled: 'bg-stone-50 text-stone-900 opacity-60 pointer-events-none',
     }
     export const defaultVariant = 'primary'
+
+    // IDEA: Add standard button sizes controlled by props
 
     export const defaultButtonClasses =
         'px-12 py-4 transform-gpu hover:scale-105 duration-100 text-xl font-semibold rounded-2xl text-center'
@@ -25,14 +27,16 @@
     export let onClick: ButtonProps['onClick']
     export let label: ButtonProps['label']
     export let autofocus: ButtonProps['autofocus'] = false
+    export let disabled: boolean = false
 </script>
 
 <!-- svelte-ignore a11y-autofocus -->
 <button
     {autofocus}
+    {disabled}
     class={cx(
         defaultButtonClasses,
-        variants[variant ?? defaultVariant],
+        variants[disabled ? 'disabled' : variant ?? defaultVariant],
         $$props.class ?? '',
     )}
     on:click={onClick}
