19ce3dfb18263399fbd1287da6fa9d8bee4e7938
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-02-06 00:01:54 +0100

    Implement rotating + and x for IDG categories

--- src/routes/focus.svelte
+++ src/routes/focus.svelte
@@ -4,7 +4,7 @@
     import {
         InnerDevelopmentGoals,
         IDGSkill,
-        category,
+        IDGCategory,
         skillsInCategory,
     } from '$lib/idgs'
     import { cx } from '$lib/utils'
@@ -34,6 +34,27 @@
         // TODO: Save choices in localStorage (or IndexedDB in the future for a app beyond the PoC stage)
         // TODO: Navigate to the next screen, using svelte's built-in router
     }
+
+    let isCategoryOpen: Record<IDGCategory['id'], boolean> = Object.values(
+        InnerDevelopmentGoals.categories,
+    ).reduce((isCategoryOpen: Record<IDGCategory['id'], boolean>, category) => {
+        isCategoryOpen[category.id] = false
+        return isCategoryOpen
+    }, {})
+    const onToggle = (
+        event: Event & {
+            currentTarget: EventTarget & HTMLElement
+        },
+        categoryId: IDGCategory['id'],
+    ) => {
+        isCategoryOpen = {
+            ...isCategoryOpen,
+            [categoryId]: (
+                event?.target as unknown as EventTarget & HTMLDetailsElement
+            ).open,
+        }
+        return isCategoryOpen
+    }
 </script>
 
 <h1 class="px-8 pt-16 text-6xl tracking-wider md:pt-20">
@@ -42,15 +63,35 @@
 
 <div class="space-y-5 py-12 md:py-16">
     {#each InnerDevelopmentGoals.categories as { name, subtitle, id: categoryId, color }}
-        <details class="text-stone-900">
+        <details
+            class={cx('text-stone-900')}
+            on:toggle={(event) => onToggle(event, categoryId)}
+        >
             <summary
                 class={cx(
-                    '!list-none marker:!hidden p-4 select-none text-lg cursor-pointer',
+                    '!list-none marker:!hidden p-4 select-none text-lg cursor-pointer flex justify-between items-center',
                     color,
                 )}
             >
-                <span class="text-xl font-bold">{name}</span>
-                <br />{subtitle}
+                <span>
+                    <span class="text-xl font-bold">{name}</span>
+                    <br />{subtitle}
+                </span>
+                <svg
+                    class={cx(
+                        'h-6 w-6 transform transition duration-150',
+                        isCategoryOpen[categoryId] ? 'rotate-0' : 'rotate-45',
+                    )}
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                        fill-rule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                    />
+                </svg>
             </summary>
             <div class="flex flex-wrap justify-center gap-3 px-4 pt-4">
                 {#each skillsInCategory(categoryId) as skill (skill.name)}
