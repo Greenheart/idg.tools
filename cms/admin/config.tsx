6d992c6676a3dd09b216b3e1554ab3f22acc75e1
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-02-11 09:04:25 +0100

    Add custom logo

---
+++ cms/admin/config.tsx
@@ -0,0 +1,11 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from '@keystone-ui/core'
+
+function CustomLogo() {
+    return <h3>IDG Tools</h3>
+}
+
+export const components = {
+    Logo: CustomLogo,
+}
