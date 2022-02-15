f3ef80da506525cdd4382711db3c27e6683c9f4e
chrismact <christophermactaggart@gmail.com>
2022-02-15 17:10:39 +0100

    added challenge
    Co-authored-by: samuel29k <samuel29k@users.noreply.github.com>

---
+++ app/src/routes/tools/[slug].svelte
@@ -0,0 +1,16 @@
+<!-- https://kit.svelte.dev/docs/routing -->
+<script lang="ts">
+    import Tool from '$components/Tool.svelte'
+
+    // TODO: show ui layout
+    // TODO: show hard coded data with always 'inner-compass'
+    // TODO: fetch dynamic tool data based on the `slug`
+    // TODO: load a random tool based on the user's selected IDGs
+
+    // TODO: replace temp hard coded value
+    const slug = 'inner-compass'
+</script>
+
+<h1 class="pt-8 text-5xl tracking-wider md:pt-16">Today's focus</h1>
+
+<Tool id={slug} />
---
+++ app/src/routes/tools/index.svelte
@@ -0,0 +1,1 @@
+<p>TODO: list all available tools here</p>
