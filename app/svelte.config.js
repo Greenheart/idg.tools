57c788a030ce23e60bc9a9f5172e9fcbaa0a5b4d
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-02-24 01:32:20 +0100

    Add adapter-vercel

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,6 +1,6 @@
 import preprocess from 'svelte-preprocess'
 import { resolve } from 'path'
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
