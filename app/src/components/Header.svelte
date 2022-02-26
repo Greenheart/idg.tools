ce001c278c8f7497a2a44d042f3797a6d34f4050
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-02-26 12:05:04 +0100

    Add hamburger menu icon

--- app/src/components/Header.svelte
+++ app/src/components/Header.svelte
@@ -1,7 +1,13 @@
-<script>
+<script lang="ts">
     import { COMMUNITY_LINK } from '$lib/constants'
 
     import Link from './Link.svelte'
+
+    let isMenuOpen = false
+
+    const toggleMenu = () => {
+        isMenuOpen = !isMenuOpen
+    }
 </script>
 
 <header class="sticky top-0 flex items-center justify-between pt-6 pb-12">
@@ -9,7 +15,18 @@
         <img src="/images/IDG-tools.svg" alt="IDG.tools logo" />
     </Link>
 
-    <nav class="flex justify-evenly">
+    <button
+        class="hamburger hamburger--spring"
+        class:is-active={isMenuOpen}
+        type="button"
+        on:click={toggleMenu}
+    >
+        <span class="hamburger-box">
+            <span class="hamburger-inner" />
+        </span>
+    </button>
+
+    <nav class="hidden justify-evenly md:flex">
         <Link class="p-2 hover:underline" href="/explore">Explore</Link>
         <!-- IDEA: Add /community page to explain the various types of communities:
             - IDG community on Element
