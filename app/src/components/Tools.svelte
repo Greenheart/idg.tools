a1271c4bc3f177506eaa9e9a1371d9475c159a84
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-02-26 17:37:20 +0100

    Simplify rendering of selected tools

---
+++ app/src/components/Tools.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import type { Content, Tool as ToolType } from '$lib/types'
+    import Tool from '$components/Tool.svelte'
+
+    export let tools: ToolType[]
+    export let content: Content
+</script>
+
+{#each tools as tool (tool.slug)}
+    <Tool {tool} {content} />
+{/each}
--- app/src/routes/explore/index.svelte
+++ app/src/routes/explore/index.svelte
@@ -4,11 +4,10 @@
     import type { Content } from '$lib/types'
     import { selectedSkills } from '$lib/stores'
     import LinkButton from '$components/LinkButton.svelte'
-    import RecommendedTools from '$components/RecommendedTools.svelte'
-    import Tool from '$components/Tool.svelte'
     import Heading from '$components/Heading.svelte'
     import { COMMUNITY_LINK } from '$lib/constants'
     import Link from '$components/Link.svelte'
+    import Tools from '$components/Tools.svelte'
 
     export let content: Content
 
@@ -17,10 +16,8 @@
     })
 </script>
 
-<!-- TODO: list all tools in grid format -->
 <!-- IDEA: filter tools based on related IDGs (possible to select one or multiple) -->
 <!-- IDEA: add basic client side search to find relevant content -->
-<!-- IDEA: at the bottom, add invitation to join the co-creation -->
 
 <Heading size={1}>My tools</Heading>
 
@@ -36,15 +33,16 @@
 <Heading class="pb-5">Library</Heading>
 
 <div class="grid gap-5">
-    {#if $selectedSkills.length}
-        <!-- TODO: load a random tool based on the user's selected IDGs. Don't show all recommended, but select a new one every day -->
-        <!-- TODO: improve recommended tools -->
-        <RecommendedTools selectedSkills={$selectedSkills} {content} />
-    {:else}
-        {#each content.tools as tool (tool.slug)}
-            <Tool {tool} {content} />
-        {/each}
-    {/if}
+    <Tools
+        tools={$selectedSkills.length
+            ? content.tools.filter((tool) =>
+                  tool.skills.some((skillId) =>
+                      $selectedSkills.includes(skillId),
+                  ),
+              )
+            : content.tools}
+        {content}
+    />
 </div>
 
 <div
