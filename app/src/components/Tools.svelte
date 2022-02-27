63a3e20e0559fe22f7cae7067c83c4e236ba54f5
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-02-27 02:31:01 +0100

    Add empty state when no relevant tools can be found

--- app/src/components/Tools.svelte
+++ app/src/components/Tools.svelte
@@ -1,11 +1,28 @@
 <script lang="ts">
     import type { Content, Tool as ToolType } from '$lib/types'
-    import Tool from '$components/Tool.svelte'
+    import { selectedSkills } from '$lib/stores'
+    import { COMMUNITY_LINK } from '$lib/constants'
+    import Tool from './Tool.svelte'
+    import Link from './Link.svelte'
+    import Button from './Button.svelte'
 
     export let tools: ToolType[]
     export let content: Content
+
+    const resetSkills = () => {
+        $selectedSkills = []
+    }
 </script>
 
 {#each tools as tool (tool.link)}
     <Tool {tool} {content} />
+{:else}
+    <div class="flex flex-col items-center space-y-4">
+        <p>
+            Sorry! No published tools for these skills yet. Welcome to join
+            <Link href={COMMUNITY_LINK} variant="pink">our community</Link> and co-create
+            more tools!
+        </p>
+        <Button on:click={resetSkills} label="View all skills" />
+    </div>
 {/each}
