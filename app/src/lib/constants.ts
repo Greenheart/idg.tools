25703bf38a11e020cd8c989ac0697f29fd1f1b03
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-03-01 10:17:06 +0100

    Update community link

--- app/src/lib/constants.ts
+++ app/src/lib/constants.ts
@@ -1,2 +1,3 @@
-export const COMMUNITY_LINK = 'https://matrix.to/#/#idg:matrix.org'
+export const COMMUNITY_LINK =
+    'https://app.element.io/#/room/#join:community.29k.org'
 export const TAGLINE = 'Inner Development Toolbox'
