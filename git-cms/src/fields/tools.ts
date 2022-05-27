bc35167c72df9927e90b1eb601466657fdef7feb
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-05-27 18:30:45 +0200

    Feature: linking tags to tools, with i18n support

--- git-cms/src/fields/tools.ts
+++ git-cms/src/fields/tools.ts
@@ -1,11 +1,25 @@
 import { CustomCmsField, DESCRIPTION, ID, NAME } from './shared'
 
-export const ToolsFields: CustomCmsField[] = [ID, NAME, DESCRIPTION]
+export const ToolsFields: CustomCmsField[] = [
+    ID,
+    NAME,
+    DESCRIPTION,
+    {
+        label: 'Tags',
+        name: 'tags',
+        multiple: true,
+        widget: 'relation',
+        collection: 'tags',
+        value_field: 'id',
+        search_fields: ['name', 'id'],
+        display_fields: ['name'],
+        i18n: true,
+    },
+]
 
 /**
  * TODO:
  *
  * - slug field for the permanent ending to the link
  * - link for storing the slugified name + slug that will be used to link to the tool
- * - tags to categorize content
  */
