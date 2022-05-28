1cda7a02dcaf96f4087c6e44addf721688daeabd
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-05-28 19:30:30 +0200

    Add skills to categories

--- git-cms/src/fields/categories.ts
+++ git-cms/src/fields/categories.ts
@@ -1,3 +1,20 @@
 import { CustomCmsField, DESCRIPTION, ID, NAME } from './shared'
 
-export const CategoriesFields: CustomCmsField[] = [ID, NAME, DESCRIPTION]
+export const CategoriesFields: CustomCmsField[] = [
+    ID,
+    NAME,
+    DESCRIPTION,
+    {
+        label: 'Skills',
+        name: 'skills',
+        multiple: true,
+        widget: 'relation',
+        collection: 'skills',
+        value_field: 'id',
+        search_fields: ['name', 'id'],
+        display_fields: ['name'],
+        i18n: true,
+        max: 23,
+        options_length: 23,
+    },
+]
