8d3ce639b40863cb440e65465ed62ffcc7725994
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-06-02 21:52:08 +0200

    Update UniqueId widget to support react refs

--- git-cms/src/widgets/UniqueId.tsx
+++ git-cms/src/widgets/UniqueId.tsx
@@ -1,30 +1,28 @@
-import React, { FC } from 'react'
+import React, { FC, RefObject } from 'react'
 import cuid from 'cuid'
 import { CmsWidgetControlProps, CmsWidgetParam } from 'netlify-cms-core'
 
-const UniqueId: FC<CmsWidgetControlProps<string>> = ({
-    value = '',
-    forID,
-    classNameWrapper,
-    onChange,
-}) => {
-    if (!value) {
-        value = cuid()
-        onChange(value)
-    }
-    return (
-        <input
-            type="text"
-            className={classNameWrapper}
-            style={{
-                color: '#cdcdcd',
-            }}
-            value={value}
-            id={forID}
-            disabled
-        />
-    )
-}
+const UniqueId: FC<CmsWidgetControlProps<string>> = React.forwardRef(
+    ({ value = '', forID, classNameWrapper, onChange }, ref) => {
+        if (!value) {
+            value = cuid()
+            onChange(value)
+        }
+        return (
+            <input
+                type="text"
+                className={classNameWrapper}
+                style={{
+                    color: '#cdcdcd',
+                }}
+                value={value}
+                id={forID}
+                ref={ref as RefObject<HTMLInputElement>}
+                disabled
+            />
+        )
+    },
+)
 
 const Widget = {
     name: 'uniqueId',
