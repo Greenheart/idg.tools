141060d1737874355f55af712035289f1eb90a18
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-06-19 18:13:38 +0200

    WIP: Implement the new tools layout with test data
    Co-authored-by: Joel Plumppu <paccao@users.noreply.github.com>

---
+++ app/src/components/DetailedRelevantSkills.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import { getCategory, getSkill } from '$lib/content-utils'
+    import type { Content, Tool } from '$shared/types'
+    import { TMP_TOOL } from '../routes/explore/[link].svelte'
+    import RelevancyScore from './RelevancyScore.svelte'
+    export const tool: Tool = TMP_TOOL['en']
+    export let content: Content
+    let className = ''
+    export { className as class }
+
+    let [mostRelevant, remaining] = [
+        tool.relevancy.slice(0, 5),
+        tool.relevancy.slice(5).length,
+    ]
+
+    let visibleSkills = mostRelevant
+    function showAll() {
+        visibleSkills = tool.relevancy
+    }
+</script>
+
+<div class={className}>
+    <div class={'grid grid-cols-1 gap-3'}>
+        {#each visibleSkills as { skill: skillId, score }}
+            {@const skill = getSkill(skillId, content)}
+            {@const category = getCategory(skill.category, content)}
+
+            <div class="flex items-center justify-between gap-4">
+                <span
+                    class="overflow-hidden text-ellipsis whitespace-nowrap rounded-lg px-2 py-1 font-semibold text-stone-900"
+                    style={`background-color: ${category.color}`}
+                >
+                    {skill.name}
+                </span>
+                <RelevancyScore {score} />
+            </div>
+        {/each}
+    </div>
+
+    {#if visibleSkills.length < tool.relevancy.length}
+        <button
+            class="mt-4 font-semibold text-[#E0A1B4] underline"
+            on:click={showAll}
+        >
+            Show {remaining ?? ''} more
+        </button>
+    {/if}
+</div>
--- app/src/components/RelevancyScore.svelte
+++ app/src/components/RelevancyScore.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
     import { cx } from '$lib/utils'
-    export let relevancy: number
+    export let score: number
     const baseClasses = 'h-6 w-6 rounded-full border border-white'
+    // This results in 1-5 dots that should be filled dependency on the relevancy
+    const numberOfDots = (Math.ceil(score / 20) * 20) / 20
 </script>
 
 <div class="flex gap-1">
-    <div class={cx(baseClasses, relevancy >= 1 ? 'bg-white' : '')} />
-    <div class={cx(baseClasses, relevancy >= 2 ? 'bg-white' : '')} />
-    <div class={cx(baseClasses, relevancy >= 3 ? 'bg-white' : '')} />
-    <div class={cx(baseClasses, relevancy >= 4 ? 'bg-white' : '')} />
-    <div class={cx(baseClasses, relevancy >= 5 ? 'bg-white' : '')} />
+    <div class={cx(baseClasses, numberOfDots >= 1 ? 'bg-white' : '')} />
+    <div class={cx(baseClasses, numberOfDots >= 2 ? 'bg-white' : '')} />
+    <div class={cx(baseClasses, numberOfDots >= 3 ? 'bg-white' : '')} />
+    <div class={cx(baseClasses, numberOfDots >= 4 ? 'bg-white' : '')} />
+    <div class={cx(baseClasses, numberOfDots >= 5 ? 'bg-white' : '')} />
 </div>
