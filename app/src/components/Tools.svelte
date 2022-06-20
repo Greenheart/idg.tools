b0cd4b0abf8d1f4eff15605255fcaed95a827ce5
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-06-20 02:56:17 +0200

    Improve limit

--- app/src/components/Tools.svelte
+++ app/src/components/Tools.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-    import type { Content, Tool as ToolType } from '$lib/types'
+    import type { Content, Tool as ToolType } from '$shared/types'
     import { selectedSkills } from '$lib/stores'
     import Tool from './Tool.svelte'
     import Button from './Button.svelte'
+    import Link from './Link.svelte'
 
     export let tools: ToolType[]
     export let content: Content
@@ -10,9 +11,15 @@
     const resetSkills = () => {
         $selectedSkills = []
     }
+
+    let limit = 3
+
+    const showMore = () => {
+        limit += 3
+    }
 </script>
 
-{#each tools as tool (tool.link)}
+{#each tools.slice(0, limit) as tool (tool.link)}
     <Tool {tool} {content} />
 {:else}
     <div class="flex flex-col items-center space-y-4">
@@ -25,3 +32,22 @@
         <Button on:click={resetSkills} label="View all tools" />
     </div>
 {/each}
+
+<div class="flex justify-center">
+    {#if limit < tools.length}
+        <button
+            on:click={showMore}
+            class="py-4 px-8 font-semibold text-[#E0A1B4] underline"
+        >
+            Show more
+        </button>
+    {:else}
+        <Link
+            href="https://github.com/Greenheart/idg.tools/discussions"
+            variant="pink"
+            class="py-4 px-8"
+        >
+            That's all so far - suggest new tools here!
+        </Link>
+    {/if}
+</div>
