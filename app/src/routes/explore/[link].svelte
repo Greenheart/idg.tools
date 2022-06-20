a6a6cbfd29cc79ca1ebcb1e185e1573948ff8341
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-06-20 08:08:46 +0200

    Fix styling

--- app/src/routes/explore/[link].svelte
+++ app/src/routes/explore/[link].svelte
@@ -14,7 +14,7 @@
 
 <Markdown source={tool.description} class="pt-8 text-lg" />
 
-<div class="my-8 rounded-2xl bg-stone-50 p-4 text-stone-900">
+<div class="mt-8 rounded-2xl bg-stone-50 p-4 text-stone-900">
     <Heading class="pb-2 text-2xl">Try it today</Heading>
     <Markdown source={tool.actions} variant="inverted" />
 </div>
