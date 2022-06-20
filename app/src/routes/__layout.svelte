f5b59801c938d671dadf4f7911c25d74d30b960d
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-06-20 02:08:01 +0200

    Dynamic background image as users go deeper into the experience

--- app/src/routes/__layout.svelte
+++ app/src/routes/__layout.svelte
@@ -1,14 +1,34 @@
-<script>
+<script lang="ts" context="module">
+    const getBgOpacity = (routeId: string) => {
+        if (routeId === 'explore/[link]') return 75
+        if (routeId === 'explore') return 65
+        return 55
+    }
+</script>
+
+<script lang="ts">
     import Header from '$components/Header.svelte'
     import { TAGLINE } from '$lib/constants'
     import { isMenuOpen } from '$lib/stores'
     import { page } from '$app/stores'
 
     import '../app.css'
+
+    $: bodyStyle = `
+<style>
+    body {
+        background: linear-gradient(0deg, #000, rgb(0 0 0 / ${getBgOpacity(
+            $page.routeId,
+        )}%));
+    }
+</style>
+`
 </script>
 
 <svelte:head>
     <title>IDG.tools | {TAGLINE}</title>
+
+    {@html bodyStyle}
 </svelte:head>
 
 <main class="mx-auto w-full max-w-lg px-5 pb-16 text-stone-50">
