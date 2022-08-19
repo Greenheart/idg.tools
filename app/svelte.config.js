c1db7fee4db08442b66057eed176c37c9fcabfc5
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-08-19 16:56:39 +0200

    Use adapter-static instead of adapter-vercel

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,6 +1,6 @@
 import preprocess from 'svelte-preprocess'
 import { resolve } from 'path'
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,6 +10,9 @@ const config = {
         alias: {
             $components: resolve('src/components'),
             $shared: resolve('../shared'),
+        },
+        prerender: {
+            default: true,
         },
     },
 }
