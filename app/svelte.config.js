dc669a02464ff06b59534223ef2750e2697c9e77
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-08-19 19:39:06 +0200

    Switch back to adapter-vercel to fix navigation issue

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,14 +1,12 @@
 import preprocess from 'svelte-preprocess'
 import { resolve } from 'path'
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-vercel'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     preprocess: [preprocess({ postcss: true })],
     kit: {
-        adapter: adapter({
-            fallback: 'index.html',
-        }),
+        adapter: adapter(),
         alias: {
             $components: resolve('src/components'),
             $shared: resolve('../shared'),
