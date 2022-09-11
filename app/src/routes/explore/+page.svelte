19e001428a765fd0821952d7ea65baabe0537204
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-09-11 12:18:58 +0200

    Improve landing page with new community section

--- app/src/routes/explore/+page.svelte
+++ app/src/routes/explore/+page.svelte
@@ -2,10 +2,8 @@
     import { onMount } from 'svelte'
 
     import { selectedSkills } from '$lib/stores'
-    import Heading from '$components/Heading.svelte'
     import Link from '$components/Link.svelte'
     import Tools from '$components/Tools.svelte'
-    import UserGroup from '$components/icons/UserGroup.svelte'
 
     import type { PageData } from './$types'
     export let data: PageData
@@ -15,9 +13,6 @@
         selectedSkills.useLocalStorage()
     })
 </script>
-
-<!-- IDEA: filter tools based on related IDGs (possible to select one or multiple) -->
-<!-- IDEA: add basic client side search to find relevant content -->
 
 <p class="pb-6">
     These are the tools we recommend based on the skills you selected. <Link
@@ -38,19 +33,3 @@
         : content.tools}
     {content}
 />
-
-<div
-    class="mt-16 flex flex-col items-center justify-between rounded-2xl bg-stone-50 p-4 text-left text-stone-900"
->
-    <UserGroup class="h-8 w-8" />
-    <Heading size={3} class="py-2">Created by and for the community</Heading>
-
-    <p class="px-4 pb-4">
-        IDG.tools is co-created by people from all around the world - join our
-        supportive community and let's practice inner development together!
-    </p>
-
-    <!-- <LinkButton href={COMMUNITY_LINK} variant="inverted">
-        Join community
-    </LinkButton> -->
-</div>
