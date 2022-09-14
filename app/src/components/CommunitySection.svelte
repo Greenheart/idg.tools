9a376293ba56709c98643ea3f35961f995010980
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-09-14 02:18:52 +0200

    Improve layout

--- app/src/components/CommunitySection.svelte
+++ app/src/components/CommunitySection.svelte
@@ -6,10 +6,12 @@
 </script>
 
 <div
-    class="mt-16 flex flex-col items-center justify-between rounded-2xl bg-stone-50 p-4 text-left text-stone-900"
+    class="mt-16 flex flex-col items-center justify-between rounded-2xl bg-stone-50 p-4 text-stone-900"
 >
     <UserGroup class="h-8 w-8" />
-    <Heading size={2} class="py-2">Created by and for the community</Heading>
+    <Heading size={2} class="py-2 text-center"
+        >Created by and for the community</Heading
+    >
 
     <!-- NOTE: THis is helpful for talking about the hubs community and similar, and might be useful in the future -->
     <!-- <p class="px-4 pb-4">
@@ -17,7 +19,7 @@
         supportive community and let's practice inner development together!
     </p> -->
 
-    <p class="px-4 pb-4">
+    <p class="px-4 pb-4 text-left">
         We’re a non-profit, open source community, always learning and improving
         this library of knowledge, together. And with your help, we can make
         IDG.tools even better!
