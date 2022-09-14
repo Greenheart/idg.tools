f8647c9f5d4216d21c4da091a060a06db7a48a89
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-09-14 03:20:07 +0200

    Fill full width with tabs

--- app/src/components/SkillTabs.svelte
+++ app/src/components/SkillTabs.svelte
@@ -17,36 +17,47 @@
     export { className as class }
 </script>
 
-<!-- TODO: Make this take up the full width of the screen -->
-<TabGroup
-    class={cx(
-        'overflow-hidden text-stone-900 sticky top-0 z-10 bg-stone-50',
-        className,
-    )}
-    on:change={() => document.querySelector('.skill-tabs')?.scrollTo(0, 0)}
->
-    <TabList class="flex flex-nowrap overflow-x-scroll sm:overflow-auto">
-        {#each content.categories as { name, id: categoryId } (categoryId)}
-            {@const color = getColor(categoryId, 'text')}
-            <Tab
-                class={({ selected }) =>
-                    cx(
-                        'py-4 px-2 text-lg first:pl-4 last:pr-4',
-                        selected ? cx(color, 'underline') : 'text-stone-900',
-                    )}>{name}</Tab
+<div class={cx('sticky top-0 z-10 h-[131px]', className)}>
+    <div class="relative">
+        <TabGroup
+            class="absolute top-0 left-0 right-0 -ml-5 -mr-5 overflow-hidden bg-stone-50 text-stone-900"
+            on:change={() =>
+                document.querySelector('.skill-tabs')?.scrollTo(0, 0)}
+        >
+            <TabList
+                class="flex flex-nowrap overflow-x-scroll sm:overflow-auto"
             >
-        {/each}
-    </TabList>
-    <TabPanels class="skill-tabs grid overflow-x-scroll">
-        {#each content.categories as { id: categoryId } (categoryId)}
-            {@const color = getColor(categoryId)}
-            <TabPanel
-                class={cx('flex flex-nowrap gap-3 bg-opacity-70 p-4', color)}
-            >
-                {#each getSkillsInCategory(categoryId, content) as skill (skill.name)}
-                    <SkillButton {skill} class="whitespace-nowrap" />
+                {#each content.categories as { name, id: categoryId } (categoryId)}
+                    {@const color = getColor(categoryId, 'text')}
+                    <Tab
+                        class={({ selected }) =>
+                            cx(
+                                'py-4 px-2 text-lg',
+                                selected
+                                    ? cx(color, 'underline')
+                                    : 'text-stone-900',
+                            )}>{name}</Tab
+                    >
                 {/each}
-            </TabPanel>
-        {/each}
-    </TabPanels>
-</TabGroup>
+            </TabList>
+            <TabPanels class="skill-tabs grid overflow-x-scroll">
+                {#each content.categories as { id: categoryId } (categoryId)}
+                    {@const color = getColor(categoryId)}
+                    <TabPanel
+                        class={cx(
+                            'flex flex-nowrap gap-2 bg-opacity-70 p-2',
+                            color,
+                        )}
+                    >
+                        {#each getSkillsInCategory(categoryId, content) as skill (skill.name)}
+                            <SkillButton
+                                {skill}
+                                class="transform-none whitespace-nowrap py-2 lg:transform-gpu"
+                            />
+                        {/each}
+                    </TabPanel>
+                {/each}
+            </TabPanels>
+        </TabGroup>
+    </div>
+</div>
