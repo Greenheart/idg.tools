1343be8ed1ddbf1d086e8e08e68a1c240d37ec36
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-09-14 03:28:34 +0200

    Hide all app content when mobile menu is opened to prevent weird background and scrolling

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -3,6 +3,7 @@
     import { TAGLINE } from '$lib/constants'
     import { page } from '$app/stores'
     import { getBgOpacity } from './+layout'
+    import { isMenuOpen } from '$lib/stores'
 
     import '../app.css'
     import CommunitySection from '$components/CommunitySection.svelte'
@@ -29,9 +30,14 @@
 
 <main class="mx-auto w-full max-w-2xl px-5 pb-16 text-stone-50 sm:max-w-5xl">
     <Header />
-    <slot />
+    <div class:hidden={$isMenuOpen}>
+        <slot />
+    </div>
 </main>
 
-<div class="mx-auto w-full max-w-2xl px-5 pb-16 sm:max-w-3xl">
+<div
+    class="mx-auto w-full max-w-2xl px-5 pb-16 sm:max-w-3xl"
+    class:hidden={$isMenuOpen}
+>
     <CommunitySection />
 </div>
