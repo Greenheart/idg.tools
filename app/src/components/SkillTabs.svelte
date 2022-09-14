9d16ff401eb81688638af2dee8e5686d3792bd69
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-09-14 03:05:24 +0200

    fix spacing

--- app/src/components/SkillTabs.svelte
+++ app/src/components/SkillTabs.svelte
@@ -20,20 +20,18 @@
 <!-- TODO: Make this take up the full width of the screen -->
 <TabGroup
     class={cx(
-        'overflow-hidden text-stone-900 sticky top-0 z-10 bg-white',
+        'overflow-hidden text-stone-900 sticky top-0 z-10 bg-stone-50',
         className,
     )}
     on:change={() => document.querySelector('.skill-tabs')?.scrollTo(0, 0)}
 >
-    <TabList
-        class="flex flex-nowrap overflow-x-scroll first:pl-2 last:pr-2 sm:overflow-auto"
-    >
+    <TabList class="flex flex-nowrap overflow-x-scroll sm:overflow-auto">
         {#each content.categories as { name, id: categoryId } (categoryId)}
             {@const color = getColor(categoryId, 'text')}
             <Tab
                 class={({ selected }) =>
                     cx(
-                        'py-4 px-2 text-lg',
+                        'py-4 px-2 text-lg first:pl-4 last:pr-4',
                         selected ? cx(color, 'underline') : 'text-stone-900',
                     )}>{name}</Tab
             >
