3bfda88cc5eca064647a16b3c38fb84912bf37ea
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-09-14 05:40:25 +0200

    Experiment with default IDG colors

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -20,21 +20,21 @@ const config = {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
             },
             // Our adapted colors
-            colors: {
-                being: '#D9C7A7',
-                thinking: '#E0A1B4',
-                relating: '#DE7268',
-                collaborating: '#F18E61',
-                acting: '#9E7983',
-            },
-            // Original IDG colors
             // colors: {
-            //     being: '#d4b88c',
-            //     thinking: '#e585a1',
-            //     relating: '#e84139',
-            //     collaborating: '#ff6821',
-            //     acting: '#661a30',
+            //     being: '#D9C7A7',
+            //     thinking: '#E0A1B4',
+            //     relating: '#DE7268',
+            //     collaborating: '#F18E61',
+            //     acting: '#9E7983',
             // },
+            // Original IDG colors
+            colors: {
+                being: '#d4b88c',
+                thinking: '#e585a1',
+                relating: '#e84139',
+                collaborating: '#ff6821',
+                acting: '#661a30',
+            },
         },
     },
     plugins: [require('@tailwindcss/typography')],
