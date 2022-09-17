ffd54c28446175612287fb6004c02351aaf25370
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-09-17 20:20:35 +0200

    Only show the relevant skills in the ToolPreview to show what the user searched for

--- app/src/components/RelevantSkills.svelte
+++ app/src/components/RelevantSkills.svelte
@@ -2,6 +2,7 @@
     import { getSkill } from '$shared/content-utils'
     import type { Content, Tool } from '$shared/types'
     import { cx, getColor } from '$lib/utils'
+    import { selectedSkills } from '$lib/stores'
 
     export let tool: Tool
     export let visible: number
@@ -11,14 +12,12 @@
 </script>
 
 <div class={cx('flex flex-wrap items-start gap-2 pr-[29px]', className)}>
-    {#each tool.relevancy.slice(0, visible) as relevancy}
+    {#each tool.relevancy
+        .filter($selectedSkills.length ? ({ skill }) => $selectedSkills.includes(skill) : () => true)
+        .slice(0, visible) as relevancy}
         {@const skill = getSkill(relevancy.skill, content)}
         {@const color = getColor(relevancy.skill)}
-        <!--
-            IDEA: OR maybe, we could show a modal with details about the skill when people click the skill.
-            This could be a way to give more details.
-            We should likely do it in a clearer way though
-        -->
+        <!-- IDEA: show "+N more" -->
         <span class={cx('rounded-lg px-2 py-1 text-white', color)}>
             {skill.name}
         </span>
