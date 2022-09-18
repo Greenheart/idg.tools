347989b48ef0f4200fa960ada3dc2143c5ba0aec
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-09-18 18:34:31 +0200

    Update localStorage loading

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -12,7 +12,7 @@
     export let data: PageData
     $: ({ content } = data)
 
-    onMount(async () => {
+    onMount(() => {
         selectedSkills.useLocalStorage()
         visibleItems.useLocalStorage()
     })
