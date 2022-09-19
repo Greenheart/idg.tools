1777da44ae89aa217d5397d7abee218392a5614f
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-09-19 15:02:03 +0200

    Improve empty states

--- app/src/components/Tools.svelte
+++ app/src/components/Tools.svelte
@@ -22,26 +22,28 @@
 {#each tools.slice(0, $visibleItems) as tool (tool.link)}
     <ToolPreview {tool} {content} />
 {:else}
-    <div class="flex flex-col items-center space-y-4 pt-16 lg:col-span-2">
+    <div class="flex flex-col items-center space-y-4 lg:col-span-2">
+        <p>No published tools matching your filters yet.</p>
+        <Button on:click={showAll}>Show all tools</Button>
         <p>
-            No published tools for these skills yet. Welcome to
-            <Link href={SUGGEST_NEW_TOOL_LINK} variant="pink"
+            Welcome to <Link href={SUGGEST_NEW_TOOL_LINK} variant="pink"
                 >suggest new tools here</Link
             >!
         </p>
-        <Button on:click={showAll}>Show all tools</Button>
     </div>
 {/each}
 
 <div
-    class="flex flex-col items-center justify-center space-y-4 pt-16 lg:col-span-2"
+    class="flex flex-col items-center justify-center space-y-4 pt-8 lg:col-span-2"
 >
     {#if $visibleItems < tools.length}
         <Button on:click={showMore}>Show more</Button>
     {:else if tools.length}
-        <p>That's everything matching your search right now.</p>
         {#if $selectedSkills.length || $selectedTags.length}
+            <p>These are all tools matching your search so far.</p>
             <Button on:click={showAll}>Show all tools</Button>
+        {:else}
+            <p>These are all published tools so far.</p>
         {/if}
         <p>
             Welcome to <Link href={SUGGEST_NEW_TOOL_LINK} variant="pink"
