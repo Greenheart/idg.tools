09e583a881125d3689ef1e6c1dc5be4fc6250d6a
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-09-27 16:12:37 +0200

    Show +N more after the most relevant skills

--- app/src/components/RelevantSkills.svelte
+++ app/src/components/RelevantSkills.svelte
@@ -9,19 +9,29 @@
     export let content: Content
     let className = ''
     export { className as class }
+
+    const mostRelevantSkills = tool.relevancy
+        .filter(
+            $selectedSkills.length
+                ? ({ skill }) => $selectedSkills.includes(skill)
+                : () => true,
+        )
+        .slice(0, visible)
 </script>
 
 <div
     class={cx('flex flex-wrap items-start gap-2 pr-[29px] text-sm', className)}
 >
-    {#each tool.relevancy
-        .filter($selectedSkills.length ? ({ skill }) => $selectedSkills.includes(skill) : () => true)
-        .slice(0, visible) as relevancy}
+    {#each mostRelevantSkills as relevancy}
         {@const skill = getSkill(relevancy.skill, content)}
         {@const color = getColor(relevancy.skill)}
-        <!-- IDEA: show "+N more" -->
         <span class={cx('rounded-lg px-2 py-1 text-white', color)}>
             {skill.name}
         </span>
     {/each}
+    {#if mostRelevantSkills.length < tool.relevancy.length}
+        <span class="self-center"
+            >+{tool.relevancy.length - mostRelevantSkills.length}</span
+        >
+    {/if}
 </div>
