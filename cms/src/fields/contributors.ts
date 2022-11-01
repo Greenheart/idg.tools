0c4de4bd0f36c886d51c926e115ad3566dedbddb
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-11-01 20:11:56 +0100

    Update contributors type

--- cms/src/fields/contributors.ts
+++ cms/src/fields/contributors.ts
@@ -3,5 +3,11 @@ import { CustomCmsField, ID, NAME } from './shared'
 export const ContributorsFields: CustomCmsField[] = [
     ID,
     NAME,
-    { label: 'Link', name: 'link', i18n: 'duplicate' },
+    {
+        label: 'Link',
+        name: 'link',
+        i18n: 'duplicate',
+        required: false,
+        hint: 'Link to personal webpage, LinkedIn or email',
+    },
 ]
