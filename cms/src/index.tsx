3d0233ad798bd303202621be7c06d44388c5d839
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-11-04 18:52:52 +0100

    Move collections to separate file

--- cms/src/index.tsx
+++ cms/src/index.tsx
@@ -1,17 +1,10 @@
 import CMS from 'netlify-cms-app'
-import { CmsCollection } from 'netlify-cms-core'
 
 import { default as UniqueId } from './widgets/UniqueId'
 import { default as UniqueSlug } from './widgets/UniqueSlug'
 
 import { DEFAULT_LANGUAGE_TAG, LANGUAGE_TAGS } from '../../shared/constants'
-import { ToolsFields } from './fields/tools'
-import { SkillsFields } from './fields/skills'
-import { DimensionsFields } from './fields/dimensions'
-import { TagsFields } from './fields/tags'
-import { StoriesFields } from './fields/stories'
-import { ContributorsFields } from './fields/contributors'
-import { FeaturedContentFields } from './fields/settings/featured'
+import { COLLECTIONS } from './collections'
 
 CMS.init({
     config: {
@@ -33,128 +26,7 @@ CMS.init({
             locales: LANGUAGE_TAGS,
             default_locale: DEFAULT_LANGUAGE_TAG,
         },
-        collections: [
-            {
-                name: 'tools',
-                label: 'Tools',
-                label_singular: 'Tool',
-                folder: '/content/src/tools',
-                fields: ToolsFields,
-                extension: 'json',
-                format: 'json',
-                create: true,
-                delete: false,
-                publish: false,
-                summary: '{{fields.name}}',
-                identifier_field: 'id',
-                slug: '{{id}}',
-                i18n: true,
-                description:
-                    'Tools are resources that help practicing the Inner Development Goals.',
-            },
-            {
-                name: 'stories',
-                label: 'Stories',
-                label_singular: 'Story',
-                folder: '/content/src/stories',
-                fields: StoriesFields,
-                extension: 'json',
-                format: 'json',
-                create: true,
-                delete: false,
-                publish: false,
-                summary: '{{fields.title}}',
-                slug: '{{id}}',
-                identifier_field: 'id',
-                i18n: true,
-                description:
-                    'Stories to share insights and learnings of how the IDGs are being used in practice, to reach the SDGs.',
-            },
-            {
-                name: 'contributors',
-                label: 'Contributors',
-                label_singular: 'Contributor',
-                folder: '/content/src/contributors',
-                fields: ContributorsFields,
-                extension: 'json',
-                format: 'json',
-                create: true,
-                delete: false,
-                publish: false,
-                summary: '{{fields.name}}',
-                identifier_field: 'id',
-                slug: '{{id}}',
-                i18n: false,
-                description:
-                    'Give credit to the people who help co-create the IDGs.',
-            },
-            {
-                name: 'tags',
-                label: 'Tags',
-                label_singular: 'Tag',
-                folder: '/content/src/tags',
-                fields: TagsFields,
-                extension: 'json',
-                format: 'json',
-                create: true,
-                delete: false,
-                publish: false,
-                summary: '{{fields.name}}',
-                identifier_field: 'id',
-                slug: '{{id}}',
-                i18n: true,
-                description:
-                    'Create tags and use them to organize different tools.',
-            },
-            {
-                i18n: true,
-                name: 'settings',
-                label: 'Settings',
-                files: [
-                    {
-                        i18n: true,
-                        label: 'Featured Content',
-                        name: 'featured',
-                        file: '/content/src/settings/featured.json',
-                        fields: FeaturedContentFields,
-                    },
-                ],
-            },
-            {
-                name: 'skills',
-                label: 'Skills',
-                label_singular: 'Skill',
-                folder: '/content/src/skills',
-                fields: SkillsFields,
-                extension: 'json',
-                format: 'json',
-                create: false,
-                delete: false,
-                publish: false,
-                summary: '{{fields.name}}',
-                identifier_field: 'id',
-                slug: '{{id}}',
-                i18n: true,
-                description: 'The 23 skills of the Inner Development Goals.',
-            },
-            {
-                name: 'dimensions',
-                label: 'Dimensions',
-                label_singular: 'Dimension',
-                folder: '/content/src/dimensions',
-                fields: DimensionsFields,
-                extension: 'json',
-                format: 'json',
-                create: false,
-                delete: false,
-                publish: false,
-                summary: '{{fields.name}}',
-                identifier_field: 'id',
-                slug: '{{id}}',
-                i18n: true,
-                description: 'The 5 dimensions of the Inner Development Goals.',
-            },
-        ] as CmsCollection[],
+        collections: COLLECTIONS,
     },
 })
 
