0202966fa5c80df8f23cf115282e0f5765d6c8f9
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-11-08 08:55:12 +0100

    Add events to the CMS

--- cms/src/fields/settings/featured.ts
+++ cms/src/fields/settings/featured.ts
@@ -4,7 +4,7 @@ export const FeaturedContentFields: CustomCmsField[] = [
     {
         name: 'stories',
         required: false,
-        label: 'Featured Stories',
+        label: '✍️ Featured Stories',
         label_singular: 'Featured Story',
         widget: 'relation',
         multiple: true,
@@ -16,5 +16,21 @@ export const FeaturedContentFields: CustomCmsField[] = [
         search_fields: ['title', 'id'],
         display_fields: ['title'],
         hint: 'Select the stories you want to feature on the IDG.community website. You can change the order by removing and adding stories in the order you want them.',
+    },
+    {
+        name: 'events',
+        required: false,
+        label: '📅 Featured Events',
+        label_singular: 'Featured Event',
+        widget: 'relation',
+        multiple: true,
+        // NOTE: Maybe we want the same featured content in all languages, or maybe not.
+        // With `duplicate` they will be consistent, but with `i18n: true` we could have variations
+        i18n: 'duplicate',
+        collection: 'events',
+        value_field: 'id',
+        search_fields: ['name', 'id'],
+        display_fields: ['name'],
+        hint: 'Select the events you want to feature on the IDG.community website. You can change the order by removing and adding stories in the order you want them.',
     },
 ]
