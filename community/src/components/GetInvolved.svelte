c9211c470e1036a2bc6f637b61103d571f3ea187
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-11-09 01:21:54 +0100

    Add stub for GetInvolved

---
+++ community/src/components/GetInvolved.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    import Link from './Link.svelte'
+
+    // IDEA: Create menu options to help people find how and where to engage.
+
+    // type Option = {
+    //     text: string
+    //     onSelected: () => void
+    // }
+
+    // type Step = {
+    //     title: string
+    //     options: Option[]
+    // }
+    // const steps: Step[] = [
+    //     {
+    //         title: '',
+    //         options: [{ onSelected }],
+    //     },
+    // ]
+
+    // let current = 0
+</script>
+
+<div class="bg-thinking h-64 rounded-2xl grid place-items-center">
+    <Link
+        href="https://www.surveymonkey.com/r/IDGwelcome"
+        variant="white"
+        class="text-2xl"
+    >
+        Get involved
+    </Link>
+</div>
