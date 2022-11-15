cd9053468dda5bcfd0218a0274b7942ed10f7f51
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-11-15 23:51:22 +0100

    Only include relevant tags

--- content/scripts/builders/community.ts
+++ content/scripts/builders/community.ts
@@ -75,19 +75,15 @@ const prepareStories = (
                 )
             }
 
-            // TODO: prepare tags once instead of doing it for every story and every language
-            const tags = translatedTags.map((tag) => {
+            // TODO: If we only load content for each language, this step can be avoided.
+            const relevantTags = translatedTags.reduce((relevantTags, tag) => {
                 const translatedTag = tag[language as Language]
-                if (translatedTag !== undefined) return translatedTag
-                throw new Error(
-                    `[content] Tag is missing translation for language "${language}": ${JSON.stringify(
-                        tag,
-                    )}`,
-                )
-            })
+                if (translatedTag) relevantTags.push(translatedTag)
+                return relevantTags
+            }, [] as Tag[])
 
             const tagsSortedAlphabetically = story.tags
-                .map((t) => getTag(t, { tags }))
+                .map((t) => getTag(t, { tags: relevantTags }))
                 .sort(sortNamesAlphabetically)
                 .map((t) => t.id)
 
@@ -177,6 +173,14 @@ const prepareTags = (
     translatedTags.filter((translatedTag) => {
         for (const [language, tag] of Object.entries(translatedTag)) {
             if (!selectedLanguages.includes(language as Language)) continue
+            if (!tag) {
+                throw new Error(
+                    `[content] Tag is missing translation for language "${language}": ${JSON.stringify(
+                        translatedTag,
+                    )}`,
+                )
+            }
+
             const tagIsUsedBySomeStory = translatedStories.some(
                 (translatedStory) =>
                     translatedStory[language as Language]?.tags?.includes?.(
@@ -257,6 +261,9 @@ export default async function buildCommunity(
         ['stories', 'events', 'contributors', 'dimensions', 'tags'],
         contentDir,
     )
+
+    // IDEA: Perhaps we could split content by language first, and then prepare content only for the languages wanted?
+    // This would allow to filter out missing content in the beginning and only implement the selectedLanguages filering in one place.
 
     const builtContent = splitContentByLang(
         prepareContent(rawContent, selectedLanguages),
