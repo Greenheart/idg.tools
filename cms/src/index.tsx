7747c09cd82a3ae9fead18b792b9ce7233b10431
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-11-16 18:49:27 +0100

    Disable global media library with CSS instead

--- cms/src/index.tsx
+++ cms/src/index.tsx
@@ -1,5 +1,4 @@
 import CMS from 'netlify-cms-app'
-import { CmsMediaLibrary } from 'netlify-cms-core'
 
 import { default as UniqueId } from './widgets/UniqueId'
 import { default as UniqueSlug } from './widgets/UniqueSlug'
@@ -7,18 +6,11 @@ import { default as UniqueSlug } from './widgets/UniqueSlug'
 import { DEFAULT_LANGUAGE_TAG, LANGUAGE_TAGS } from '../../shared/constants'
 import { COLLECTIONS } from './collections'
 
-// HACK: https://github.com/netlify/netlify-cms/issues/1702#issuecomment-619564904
-CMS.registerMediaLibrary({
-    name: 'disabled',
-    init: () => ({ show: () => undefined, enableStandalone: () => false }),
-} as CmsMediaLibrary)
-
 CMS.init({
     config: {
         load_config_file: false,
         editor: { preview: false },
-        media_library: { name: 'disabled' },
-        // media_folder: '/tools/static/images',
+        media_folder: '/tools/static/images',
         backend: {
             name: 'github',
             repo: 'Greenheart/idg.tools',
@@ -28,7 +20,7 @@ CMS.init({
         local_backend: {
             url: 'http://localhost:2030/api/v1',
         },
-        publish_mode: 'editorial_workflow',
+        publish_mode: 'simple',
         i18n: {
             structure: 'single_file',
             locales: LANGUAGE_TAGS,
