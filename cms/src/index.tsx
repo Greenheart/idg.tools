e18b5eda0d3c8dca0fd3f5122335e0509dab0aea
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-11-16 18:44:30 +0100

    Disable CMS global media library

--- cms/src/index.tsx
+++ cms/src/index.tsx
@@ -1,4 +1,5 @@
 import CMS from 'netlify-cms-app'
+import { CmsMediaLibrary } from 'netlify-cms-core'
 
 import { default as UniqueId } from './widgets/UniqueId'
 import { default as UniqueSlug } from './widgets/UniqueSlug'
@@ -6,15 +7,18 @@ import { default as UniqueSlug } from './widgets/UniqueSlug'
 import { DEFAULT_LANGUAGE_TAG, LANGUAGE_TAGS } from '../../shared/constants'
 import { COLLECTIONS } from './collections'
 
+// HACK: https://github.com/netlify/netlify-cms/issues/1702#issuecomment-619564904
+CMS.registerMediaLibrary({
+    name: 'disabled',
+    init: () => ({ show: () => undefined, enableStandalone: () => false }),
+} as CmsMediaLibrary)
+
 CMS.init({
     config: {
         load_config_file: false,
         editor: { preview: false },
-        // TODO: Need to find a solution to avoid duplicating images across multiple sites.
-        // Ideally we can store all images in content/static/images and copy them to tools and community when building.
-        // This way, the CMS can store all images in one place, organized based on which project uses them.
-        // IDEA: Maybe hide the global media library from the navigation to avoid confusion.
-        media_folder: '/tools/static/images',
+        media_library: { name: 'disabled' },
+        // media_folder: '/tools/static/images',
         backend: {
             name: 'github',
             repo: 'Greenheart/idg.tools',
