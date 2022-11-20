e9a1f92d98da8752783eb2e8eb3cab8c164e724d
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-11-20 11:53:36 +0100

    Remove old code and format

--- community/src/routes/+page.svelte
+++ community/src/routes/+page.svelte
@@ -22,31 +22,26 @@
                 class="mt-6 font-bold underline">Inner Development Goals</Link
             > (IDGs) to achieve the <Link
                 href="https://sdgs.un.org/goals#goals"
-                class="font-bold underline"
-                >UN Sustainable Development Goals</Link
+                class="font-bold underline">UN Sustainable Development Goals</Link
             > (SDGs).
         </p>
 
         <div class="flex gap-4 pt-8">
-            <LinkButton href="#about" variant="secondary">Learn more</LinkButton
-            >
-            <LinkButton href="#participate" variant="primary"
-                >Get involved</LinkButton
-            >
+            <LinkButton href="#about" variant="secondary">Learn more</LinkButton>
+            <LinkButton href="#participate" variant="primary">Get involved</LinkButton>
         </div>
     </div>
 </div>
 
-<p class="text-center uppercase text-sm">Our three pillars:</p>
+<p class="text-center text-sm uppercase">Our three pillars:</p>
 
-<div class="grid grid-cols-3 gap-8 mt-8">
+<div class="mt-8 grid grid-cols-3 gap-8">
     <div class="text-center">
         <Heading size={2}>Tools</Heading>
         <p class="my-4">
-            We're creating an open source library of scientifically-validated
-            tools to develop the skills outlined by the IDG framework. Here you
-            can find exercises, workshops and resources relevant to the skills
-            you want to practice.
+            We're creating an open source library of scientifically-validated tools to develop the
+            skills outlined by the IDG framework. Here you can find exercises, workshops and
+            resources relevant to the skills you want to practice.
         </p>
         <Link href="https://idg.tools" variant="pink">Explore IDG.tools</Link>
     </div>
@@ -54,9 +49,9 @@
     <div class="text-center">
         <Heading size={2}>Community</Heading>
         <p class="my-4">
-            With hubs in almost 100 cities, this is an emerging network eager to
-            participate, explore and co-create. Join local events and use our
-            chat rooms to connect and learn together with a global network.
+            With hubs in almost 100 cities, this is an emerging network eager to participate,
+            explore and co-create. Join local events and use our chat rooms to connect and learn
+            together with a global network.
         </p>
         <Link href="#" variant="pink">Find your community</Link>
     </div>
@@ -64,61 +59,37 @@
     <div class="text-center">
         <Heading size={2}>Stories</Heading>
         <p class="my-4">
-            Read stories of how people around the world are using the IDG
-            framework in practice. Learn from others and try it with your local
-            hub. Share your own story and help make the IDG resources even
-            better!
+            Read stories of how people around the world are using the IDG framework in practice.
+            Learn from others and try it with your local hub. Share your own story and help make the
+            IDG resources even better!
         </p>
         <Link href="/stories" variant="pink">Inspire and get inspired</Link>
     </div>
 </div>
 
 <p>
-    We're launching the IDG.community stories as an experiment. However, we need
-    your help to sustain this, and realize the full potential. Therefore, we
-    like to invite anyone who want to see more IDG community stories in the
-    future to help out. This is a community-driven project, and a great
-    opportunity to make a meaningful contribution to further help people engage
-    with the IDGs to reach the SDGs.
+    We're launching the IDG.community stories as an experiment. However, we need your help to
+    sustain this, and realize the full potential. Therefore, we like to invite anyone who want to
+    see more IDG community stories in the future to help out. This is a community-driven project,
+    and a great opportunity to make a meaningful contribution to further help people engage with the
+    IDGs to reach the SDGs.
 </p>
 
-<div class="grid gap-x-8 py-12 mt-8">
+<div class="mt-8 grid gap-x-8 py-12">
     <Heading size={2} class="mb-4">Featured stories</Heading>
-    <div class="bg-relating h-64 rounded-2xl p-4 flex flex-col justify-between">
+    <div class="bg-relating flex h-64 flex-col justify-between rounded-2xl p-4">
         <div class="flex flex-col space-y-2">
             {#each content.stories as story}
                 <Link
                     href={`/stories/${story.link}`}
                     variant="black"
-                    class="no-underline grid grid-cols-[min-content_1fr] gap-x-2 bg-stone-50 p-2 rounded-lg shadow-lg"
+                    class="grid grid-cols-[min-content_1fr] gap-x-2 rounded-lg bg-stone-50 p-2 no-underline shadow-lg"
                 >
                     📝
                     <span>{story.title}</span>
                 </Link>
             {/each}
         </div>
-        <Link href="/stories" class="self-end" variant="black"
-            >See all &rarr</Link
-        >
+        <Link href="/stories" class="self-end" variant="black">See all &rarr</Link>
     </div>
-</div>
-
-<div class="grid py-12 w-1/2 mx-auto">
-    <Heading size={2} class="mb-2">Find your community</Heading>
-    <p class="mb-4">
-        (This could be an interactive widget to help people find their way of
-        engaging by answering a few questions, and getting recommended specific
-        actions)
-    </p>
-    <GetInvolved />
-</div>
-
-<div class="grid py-12 w-1/2 mx-auto">
-    <Heading size={2} class="mb-2">Map: Find your local IDG hub!</Heading>
-    <p class="mb-4">
-        (This could be an interactive map to visualize the key information about
-        all active IDG hubs. The data would be updated by the IDG hubs
-        themselves, as part of their commitment to remain listed)
-    </p>
-    <div class="bg-collaborating h-64 rounded-2xl" />
 </div>
