254d0df57c05b9d0df67639166862c377042cbec
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-11-21 03:22:29 +0100

    Update arrow to make it more useful

--- shared/icons/Arrow.svelte
+++ shared/icons/Arrow.svelte
@@ -3,6 +3,11 @@
 
     let className = ''
     export { className as class }
+
+    export let left = false
+    export let right = false
 </script>
 
-<span class={cx(className, '!text-xl font-medium')}>→</span>
+<span class={cx(className, '!text-xl font-medium')}
+    >{#if left}←{/if}{#if right}→{/if}</span
+>
