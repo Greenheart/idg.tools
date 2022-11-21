35b916a4cae56850d2706c5e9f3f31bf6967129e
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-11-21 04:19:30 +0100

    Fix layout for tools

--- tools/src/routes/[link]/+page.svelte
+++ tools/src/routes/[link]/+page.svelte
@@ -17,31 +17,33 @@
 
 <Heading size={1} class="pt-8">{tool.name}</Heading>
 
-<Tags {tags} visible={3} class="pt-4" inverted size="md" />
+<div class="mx-auto max-w-3xl">
+    <Tags {tags} visible={3} class="pt-4" inverted size="md" />
 
-{#if tool.intro}
-    <Markdown source={tool.intro} class="pt-4 font-bold" />
-{/if}
+    {#if tool.intro}
+        <Markdown source={tool.intro} class="pt-4 font-bold" />
+    {/if}
 
-<Markdown source={tool.description} formatting="limited" class="pt-8" />
+    <Markdown source={tool.description} formatting="limited" class="pt-8" />
 
-<div class="mt-8 rounded-2xl bg-stone-50 p-4 text-stone-900">
-    <Heading class="pb-2 text-2xl">How to practice</Heading>
-    <Markdown source={tool.actions} variant="inverted" />
-</div>
+    <div class="mt-8 rounded-2xl bg-stone-50 p-4 text-stone-900">
+        <Heading class="pb-2 text-2xl">How to practice</Heading>
+        <Markdown source={tool.actions} variant="inverted" />
+    </div>
 
-<Heading class="pt-8 pb-2">Most relevant skills</Heading>
-<DetailedRelevantSkills {skills} relevancy={tool.relevancy} />
+    <Heading class="pt-8 pb-2">Most relevant skills</Heading>
+    <DetailedRelevantSkills {skills} relevancy={tool.relevancy} />
 
-{#if tool.resources}
-    <Heading class="pt-8 pb-2">Research and resources</Heading>
-    <Resources {tool} />
-{/if}
+    {#if tool.resources}
+        <Heading class="pt-8 pb-2">Research and resources</Heading>
+        <Resources {tool} />
+    {/if}
 
-<div class="mt-8">
-    <FeedbackForm />
-</div>
+    <div class="mt-8">
+        <FeedbackForm />
+    </div>
 
-<div class="mt-8 flex justify-center">
-    <Link href="/" variant="pink">See more tools</Link>
+    <div class="mt-8 flex justify-center">
+        <Link href="/" variant="pink">See more tools</Link>
+    </div>
 </div>
