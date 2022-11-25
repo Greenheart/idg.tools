a8c46f4e1d3efbbb4b561a8b1f98487704d2c7c3
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-11-25 04:34:05 +0100

    Simplify state

--- community/src/lib/stores.ts
+++ community/src/lib/stores.ts
@@ -1,39 +1,7 @@
 import { writable } from 'svelte/store'
 
-import type { Dimension, ItemId } from '$shared/types'
-import { DIMENSION_IDS } from '$shared/constants'
+import type { Dimension, Tag } from '$shared/types'
 
-function createPersistedStore<T>(key: string, startValue: T) {
-    const { subscribe, set } = writable(startValue)
-
-    return {
-        subscribe,
-        set,
-        useLocalStorage: () => {
-            const json = localStorage.getItem(key)
-            if (typeof json === 'string') {
-                let data: T
-                try {
-                    data = JSON.parse(json)
-                } catch (error) {
-                    data = startValue
-                }
-                set(data)
-            }
-
-            subscribe((current) => {
-                localStorage.setItem(key, JSON.stringify(current))
-            })
-        },
-    }
-}
-
-export const selectedTags = createPersistedStore<ItemId[]>('selectedTags', [])
+export const selectedTags = writable<Tag['id'][]>([])
+export const selectedDimensions = writable<Dimension['id'][]>([])
 export const isMenuOpen = writable<boolean>(false)
-
-export const isDimensionOpen = writable<Record<Dimension['id'], boolean>>(
-    DIMENSION_IDS.reduce((isDimensionOpen: Record<Dimension['id'], boolean>, dimensionId) => {
-        isDimensionOpen[dimensionId] = false
-        return isDimensionOpen
-    }, {}),
-)
