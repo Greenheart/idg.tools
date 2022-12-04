9e1ed02389dfc86bc8601a26f3c5b69349470f86
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-12-04 20:10:05 +0100

    Cleanup

--- community/scripts/optimize-images.js
+++ community/scripts/optimize-images.js
@@ -9,8 +9,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 console.log('🌅 Optimizing images...')
 const images = await FastGlob(resolve(__dirname, '../source-images', '*.{jpg,png}'))
 
-// const outputDir = resolve(__dirname, '../static/images')
-const outputDir = resolve(__dirname, '../optimized-images')
+const outputDir = resolve(__dirname, '../static/images')
 
 /**
  * Get the optimal quality for a given file size
@@ -33,40 +32,6 @@ const getFilenameWithoutExtension = (path) => basename(path, extname(path))
 const existing = (await FastGlob(resolve(outputDir, '*.{jpg,webp}'))).map((image) =>
     basename(image),
 )
-
-/*
-
-
-images = getImages(inputDir)
-uniqueSourceImages = images.map(getFilenameWithoutExtension)
-
-existingImages = getImages(outputDir).map(basename) // keep extension to know which types exist
-
-for image of images
-    for type of webp, jpg
-        if !existingImages.includes(`${getFilenameWithoutExtension(image)}.${type}`)
-            await buildImage(image, type, outputDir)
-
-outputTypes = ['webp', 'jpg']
-
-await Promise.all(images.flatMap(image =>
-    outputTypes.map(type => {
-        if (!existingImages.includes(`${getFilenameWithoutExtension(image)}.${type}`)) {
-            return buildImage(image, type, outputDir)
-        }
-        return null
-    })
-))
-
-*/
-
-// const uniqueSourceImages = existing.map(getFilenameWithoutExtension)
-
-// // Skip images that already exist at output destination
-// const unique = images.filter((image) => {
-//     if (uniqueSourceImages.includes(getFilenameWithoutExtension(image))) {
-//     }
-// })
 
 console.log({ images, existing })
 
@@ -134,42 +99,3 @@ await Promise.all(
         return Promise.all(builtImages)
     }),
 )
-
-// await Promise.all(
-//     images.map(async (image) => {
-//         const input = sharp(image).rotate()
-//         const meta = await input.metadata()
-
-//         const width = Math.min(1000, meta.width)
-//         const height = Math.min(750, meta.height)
-//         const size = (await stat(image)).size
-//         const quality = getImageQuality(size)
-
-//         const webp = input
-//             .resize(width, height)
-//             .webp({ effort: 6, quality })
-//             .toBuffer()
-//             .then((buffer) =>
-//                 writeFile(resolve(outputDir, `${basename(image, extname(image))}.webp`), buffer, {
-//                     encoding: 'binary',
-//                     flag: 'w',
-//                 }),
-//             )
-//             .catch((err) => console.error('Image transformation failed', err))
-
-//         const jpeg = input
-//             .resize(width, height)
-//             .jpeg({ mozjpeg: true, quality })
-//             .toBuffer()
-//             .then((buffer) =>
-//                 writeFile(resolve(outputDir, `${basename(image, extname(image))}.jpg`), buffer, {
-//                     encoding: 'binary',
-//                     flag: 'w',
-//                 }),
-//             )
-//             .catch((err) => console.error('Image transformation failed', err))
-
-//         return webp
-//         // return Promise.all([jpeg, webp])
-//     }),
-// )
