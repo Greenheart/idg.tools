41fa687c03a39b301c29afb9214f7258f56c3223
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-12-10 13:10:39 +0100

    Use shared MenuButton for both sites

---
+++ shared/components/MenuButton.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { onKeydown } from '../utils'
+
+    export let onToggle: () => void
+    export let isOpen: boolean
+</script>
+
+<button
+    class="hamburger hamburger--spring z-10 !-mr-4 justify-evenly sm:hidden"
+    class:is-active={isOpen}
+    on:click={onToggle}
+    on:keydown={onKeydown(onToggle)}
+>
+    <span class="hamburger-box">
+        <span class="hamburger-inner" />
+    </span>
+</button>
