44bab6b45feefa1f7a93b17bc3b37d8b91b7ff72
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-12-10 11:30:23 +0100

    Remove unused code

--- tools/src/lib/utils.ts
+++ tools/src/lib/utils.ts
@@ -1,6 +1,1 @@
 export * from '$shared/utils'
-
-export const _getBgOpacity = (routeId: string) => {
-    if (routeId === '[link]') return 75
-    return 55
-}
--- tools/src/routes/+layout.ts
+++
@@ -1,1 +0,0 @@
-export { _getBgOpacity } from '$lib/utils'
