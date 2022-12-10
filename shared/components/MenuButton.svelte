b765566839b951e36c7aae3653a09b01a821b0a2
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-12-10 13:16:37 +0100

    Move menu styles to be in one place

--- shared/components/MenuButton.svelte
+++ shared/components/MenuButton.svelte
@@ -15,3 +15,103 @@
         <span class="hamburger-inner" />
     </span>
 </button>
+
+<style lang="postcss">
+    /*!
+     * Adapted from Hamburgers (MIT license)
+     * @description Tasty CSS-animated hamburgers
+     * @author Jonathan Suh @jonsuh
+     * @site https://jonsuh.com/hamburgers
+     * @link https://github.com/jonsuh/hamburgers
+     */
+    .hamburger {
+        padding: 15px 15px;
+        display: inline-block;
+        cursor: pointer;
+        transition-property: opacity, filter;
+        transition-duration: 0.15s;
+        transition-timing-function: linear;
+        font: inherit;
+        color: inherit;
+        text-transform: none;
+        background-color: transparent;
+        border: 0;
+        margin: 0;
+        overflow: visible;
+    }
+    .hamburger.is-active .hamburger-inner,
+    .hamburger.is-active .hamburger-inner::before,
+    .hamburger.is-active .hamburger-inner::after {
+        @apply bg-stone-50;
+    }
+
+    .hamburger-box {
+        width: 40px;
+        height: 24px;
+        display: inline-block;
+        position: relative;
+    }
+
+    .hamburger-inner {
+        display: block;
+        top: 50%;
+        margin-top: -2px;
+    }
+    .hamburger-inner,
+    .hamburger-inner::before,
+    .hamburger-inner::after {
+        width: 40px;
+        height: 2px;
+        position: absolute;
+        @apply bg-stone-50;
+        transition-property: transform;
+        transition-duration: 0.15s;
+        transition-timing-function: ease;
+    }
+    .hamburger-inner::before,
+    .hamburger-inner::after {
+        content: '';
+        display: block;
+    }
+    .hamburger-inner::before {
+        top: -10px;
+    }
+    .hamburger-inner::after {
+        bottom: -10px;
+    }
+
+    /*
+* Spring
+*/
+    .hamburger--spring .hamburger-inner {
+        top: 2px;
+        transition: background-color 0s 0.13s linear;
+    }
+    .hamburger--spring .hamburger-inner::before {
+        top: 10px;
+        transition: top 0.1s 0.2s cubic-bezier(0.33333, 0.66667, 0.66667, 1),
+            transform 0.13s cubic-bezier(0.55, 0.055, 0.675, 0.19);
+    }
+    .hamburger--spring .hamburger-inner::after {
+        top: 20px;
+        transition: top 0.2s 0.2s cubic-bezier(0.33333, 0.66667, 0.66667, 1),
+            transform 0.13s cubic-bezier(0.55, 0.055, 0.675, 0.19);
+    }
+
+    .hamburger--spring.is-active .hamburger-inner {
+        transition-delay: 0.22s;
+        background-color: transparent !important;
+    }
+    .hamburger--spring.is-active .hamburger-inner::before {
+        top: 0;
+        transition: top 0.1s 0.15s cubic-bezier(0.33333, 0, 0.66667, 0.33333),
+            transform 0.13s 0.22s cubic-bezier(0.215, 0.61, 0.355, 1);
+        transform: translate3d(0, 10px, 0) rotate(45deg);
+    }
+    .hamburger--spring.is-active .hamburger-inner::after {
+        top: 0;
+        transition: top 0.2s cubic-bezier(0.33333, 0, 0.66667, 0.33333),
+            transform 0.13s 0.22s cubic-bezier(0.215, 0.61, 0.355, 1);
+        transform: translate3d(0, 10px, 0) rotate(-45deg);
+    }
+</style>
