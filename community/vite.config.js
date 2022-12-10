e270618af1c2e1ca43e6be79df83bba97095bbb5
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-12-10 10:54:34 +0100

    Use verbatim dns resolution
    Co-authored-by: chrismact <chrismact@users.noreply.github.com>

--- community/vite.config.js
+++ community/vite.config.js
@@ -1,5 +1,8 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
+import { setDefaultResultOrder } from 'dns'
+
+setDefaultResultOrder('verbatim')
 
 export default defineConfig({
     plugins: [sveltekit()],
