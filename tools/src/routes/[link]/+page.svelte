996ac3caa5a44127cb059509746270e20da25f34
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-12-10 13:38:46 +0100

    Tweak styles for tool pages

--- tools/src/routes/[link]/+page.svelte
+++ tools/src/routes/[link]/+page.svelte
@@ -34,7 +34,7 @@
 
 <Markdown source={tool.description} formatting="limited" class="pt-8" />
 
-<div class="mt-8 bg-stone-50 p-4 text-stone-900">
+<div class="mt-8 bg-stone-50 p-4 text-stone-900 sm:p-8">
     <Heading class="pb-2 text-2xl">How to practice</Heading>
     <Markdown source={tool.actions} variant="inverted" formatting="limited" />
 </div>
