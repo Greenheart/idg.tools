454c339f69f53a5c95a74a545513bb3c01cd60f9
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-12-11 00:32:34 +0100

    Update tools landing page copy and design

--- tools/src/routes/+page.svelte
+++ tools/src/routes/+page.svelte
@@ -35,21 +35,27 @@
 <div class="py-12 text-lg sm:text-xl">
     <p>Welcome to the Inner Development Goals Toolkit!</p>
 
-    <!-- TODO: Update copy for the intro -->
     <p class="mt-4">
-        This is an emerging network of IDG Hubs who are using the <Link
+        This is an emerging library of tools to explore the <Link
             href={FRAMEWORK_LINK}
             class="mt-6 font-bold underline">Inner Development Goals</Link
-        > (IDGs) to achieve the <Link
+        > (IDGs) in practice, and help people and organizations reach the <Link
             href="https://sdgs.un.org/goals#goals"
             class="font-bold underline">UN Sustainable Development Goals</Link
         > (SDGs).
     </p>
 
+    <div class="mt-8 bg-stone-50 p-4 text-stone-900">
+        This early version is primarily based on the <Link
+            href="/assets/220927_IDG_Toolkit_v6.pdf"
+            class="font-underline font-bold"
+            variant="black">IDG Phase 2 Research Report</Link
+        >. Now, you're invited to explore the IDGs in practice, and co-create the future toolkit!
+    </div>
 
     <div class="mt-8 flex flex-wrap justify-center gap-x-4 gap-y-2 sm:justify-start">
         <LinkButton href="#about" variant="secondary">Learn more</LinkButton>
-        <LinkButton href="/#explore" variant="primary">Explore tools</LinkButton>
+        <LinkButton href="#explore" variant="primary">Explore tools</LinkButton>
     </div>
 </div>
 
@@ -65,8 +71,7 @@
             skills outlined by the IDG framework. Here you can find methods, workshops and resources
             relevant to the skills you want to practice.
         </p>
-        <!-- <Link href="https://idg.tools" variant="pink">Explore IDG.tools</Link> -->
-        <Link href="/#explore" variant="pink">Explore IDG.tools</Link>
+        <Link href="#explore" variant="pink">Explore tools</Link>
     </div>
 
     <div class="border-collaborating flex flex-col border-2 p-4">
@@ -87,34 +92,6 @@
         <Link href={COMMUNITY_LINK} variant="pink">Find your community</Link>
     </div>
 </div>
-
-<!-- <div class="space-y-4 pt-12 pb-16 text-lg sm:text-xl">
-    <p>
-        IDG.tools is the open source toolkit from the <Link
-            href="https://www.innerdevelopmentgoals.org/framework"
-            class="mt-6 font-bold underline">Inner Development Goals</Link
-        > (IDGs) non-profit initiative. The initiative is working to achieve the <Link
-            href="https://sdgs.un.org/goals#goals"
-            class="font-bold underline">UN Sustainable Development Goals</Link
-        > (SDGs) through inner development.
-    </p>
-
-    <p>
-        We’re creating a library of scientifically-validated methods to develop the skills outlined
-        by the IDG framework. Just select which ones you want to practice and find tools relevant
-        for you.
-    </p>
-
-    <p>
-        This toolkit is co-created by and for the community, and primarily contains methods from the <Link
-            href="/assets/220927_IDG_Toolkit_v6.pdf"
-            class="font-underline font-bold">IDG Phase 2 Research Report</Link
-        >. Next, we want to add more tools and resources, and make IDG.tools even better. <Link
-            href={FEEDBACK_FORM_LINK}
-            class="font-underline font-bold">With your support</Link
-        >, we can make it happen!
-    </p>
-</div> -->
 
 <Heading size={2} class="pb-4 pt-16">1. Choose skills to practice</Heading>
 <Filters {content} />
