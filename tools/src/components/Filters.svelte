48ba3d8c998ec623523d7677e1d1878b912a56b3
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-12-11 19:08:07 +0100

    WIP: Filters redesign

--- tools/src/components/Filters.svelte
+++ tools/src/components/Filters.svelte
@@ -12,9 +12,10 @@
     import Button from '$shared/components/Button.svelte'
     import Skills from './Skills.svelte'
     import FiltersToolbar from './FiltersToolbar.svelte'
-    import { onKeydown, pluralize } from '$lib/utils'
+    import { cx, getColor, onKeydown, pluralize } from '$lib/utils'
     import Heading from '$shared/components/Heading.svelte'
     import Tags from './Tags.svelte'
+    import LinkButton from '$shared/components/LinkButton.svelte'
 
     export let content: ToolsContent
 
@@ -46,6 +47,25 @@
                 {} as Record<Dimension['id'], boolean>,
             )
         }
+    }
+
+    const toggleSkillsInDimension = (dimension: Dimension) => {
+        console.log('toggling selectedSkills')
+
+        if (dimension.skills.every((skillId) => $selectedSkills.includes(skillId))) {
+            $selectedSkills = $selectedSkills.filter((skillId) =>
+                dimension.skills.includes(skillId),
+            )
+        } else {
+            // TODO: we might need to filter out duplicates here
+            $selectedSkills = [...$selectedSkills, ...dimension.skills]
+        }
+    }
+
+    const openFilters = () => {
+        console.log('opening filters')
+
+        $filtersExpanded = true
     }
 
     let applyButton: HTMLButtonElement
@@ -85,11 +105,39 @@
     </div>
 </Dialog>
 
+<!-- TODO: Show a instruction explaining how it works, and where to learn more about the IDG framework -->
+
+<!-- IDEA: maybe use horizontal scrolling with tabs like the old design -->
+<!-- IDEA: Just add a button to toggle all skills within a dimension -->
+<!-- This would reduce the need for advanced filters -->
+<!-- One question with this approach though is that we need to filter based on conetent tags -->
+
 <!-- IDEA: allow peple to filter tools based on dimension, and sort by the most relevant in each dimension -->
 
 <!-- TODO: display a button for each dimension, that when clicked selects all skills within that category -->
-<!-- TODO: Show a instruction explaining how it works, and where to learn more about the IDG framework -->
 <!-- TODO: Add button to open advanced filters, which opens the fullscreen Filters dialog -->
 <!-- IDEA: maybe use a `Set` instead of an `Array` to store the selectedSkills state. This migh help when selecting all skills in a specific dimension -->
 <!-- IDEA: possibly update the and/or logic of skills and tags in the filters and getMostRelevantTools() -->
-<FiltersToolbar {title} class={$filtersExpanded ? 'invisible' : ''} />
+
+<div class="flex items-center justify-between">
+    <div class="flex gap-2">
+        {#each content.dimensions as dimension}
+            <Button
+                size="md"
+                on:click={onKeydown(() => toggleSkillsInDimension(dimension))}
+                class={cx(getColor(dimension.id), 'text-white hover:bg-opacity-75')}
+                variant="unstyled"
+            >
+                {dimension.name}
+            </Button>
+        {/each}
+    </div>
+    <LinkButton
+        size="sm"
+        on:click={onKeydown(openFilters)}
+        variant="unstyled"
+        class="font-bold underline">Advanced filters</LinkButton
+    >
+</div>
+
+<!-- <FiltersToolbar {title} class={$filtersExpanded ? 'invisible' : ''} /> -->
