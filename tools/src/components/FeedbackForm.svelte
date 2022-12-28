793229de4196983c7a8701baa177598da766b562
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2022-12-28 13:32:39 +0100

    Remove unused attributes

--- tools/src/components/FeedbackForm.svelte
+++ tools/src/components/FeedbackForm.svelte
@@ -28,11 +28,11 @@
         }}
     >
         <label for="description" class="absolute -left-full">Description</label>
-        <textarea type="text" name="description" class="absolute -left-full p-2 shadow-md" />
+        <textarea name="description" class="absolute -left-full p-2 shadow-md" />
         <label for="liked" class="font-bold">What do you like about this tool?</label>
-        <textarea type="text" name="liked" bind:value={liked} class="p-2 shadow-md" />
+        <textarea name="liked" bind:value={liked} class="p-2 shadow-md" />
         <label for="improve" class="font-bold">What can be improved for this tool?</label>
-        <textarea type="text" name="improve" bind:value={improve} class="p-2 shadow-md" />
+        <textarea name="improve" bind:value={improve} class="p-2 shadow-md" />
 
         <p class="py-4 text-sm">
             Please note that your feedback will be public on <Link
