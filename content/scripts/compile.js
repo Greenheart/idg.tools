f4e20ead193e6c3c054207480c4fe43a08c252d7
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-02-08 19:17:44 +0100

    Don't compile temprary scripts

--- content/scripts/compile.js
+++ content/scripts/compile.js
@@ -8,7 +8,7 @@ esbuild
     .build({
         entryPoints: [
             resolve(__dirname, 'build-content.ts'),
-            resolve(__dirname, 'update-colors.ts'),
+            // resolve(__dirname, 'update-colors.ts'),
         ],
         outdir: resolve(__dirname, 'compiled'),
         bundle: true,
