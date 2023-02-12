e0e97356b9fb6440da4e9c68a900afd95110eec3
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-02-12 15:31:32 +0100

    Create outline for slides

---
+++ shared/components/IDGColors.svelte
@@ -0,0 +1,7 @@
+<div class="w-4 flex-col">
+    <div class="bg-being h-16 w-4" />
+    <div class="bg-thinking h-16 w-4" />
+    <div class="bg-relating h-16 w-4" />
+    <div class="bg-collaborating h-16 w-4" />
+    <div class="bg-acting h-16 w-4" />
+</div>
