3c38fc36456fe591bc2d9a7dd6f0ddc5c074c29a
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-02-23 00:58:10 +0100

    Add localized version of the IDG Framework

--- tools/src/lib/content-backend.ts
+++ tools/src/lib/content-backend.ts
@@ -1,13 +1,13 @@
 import _content from '../../static/content.json'
-import type { Locale, ToolsContent } from '$shared/types'
+import type { ToolsContent } from '$shared/types'
 import { DEFAULT_LOCALE_IDENTIFIER } from '$shared/constants'
+import { getLocale } from '$shared/content-utils'
 
 // NOTE: This is only available in SvelteKit endpoints since `content.json` is a file
-// NOTE: Hardcoding English locale content for now, but we can add support for more translations in the future.
 export const content = _content[DEFAULT_LOCALE_IDENTIFIER] as unknown as ToolsContent
 
 /**
  * Gets the content bundle for a specific locale.
  */
-export const getContent = (locale: Locale = DEFAULT_LOCALE_IDENTIFIER) =>
-    _content[locale] as unknown as ToolsContent
+export const getContent = (locale?: string) =>
+    _content[getLocale(locale)] as unknown as ToolsContent
