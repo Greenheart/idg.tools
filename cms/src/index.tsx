1e30487b2810dcbbec95174c1cbfb0f992fb8534
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-02-22 11:48:16 +0100

    Update CMS auth config

--- cms/src/index.tsx
+++ cms/src/index.tsx
@@ -6,6 +6,8 @@ import { default as UniqueSlug } from './widgets/UniqueSlug'
 import { DEFAULT_LOCALE_IDENTIFIER, LOCALE_IDENTIFIERS } from '../../shared/constants'
 import { COLLECTIONS } from './collections'
 
+// IDEA: Maybe replace the Netlify CMS logo with IDG logo, which would be very nice
+
 CMS.init({
     config: {
         load_config_file: false,
@@ -13,9 +15,17 @@ CMS.init({
         media_folder: '/tools/static/images',
         backend: {
             name: 'github',
+            /**
+             * TODO: investigate if we can use open authoring to let anyone edit and open PRs with their GitHub account
+             * This requires us to add two more config fields, but is much better UX than making us into gatekeepers for who can edit or not.
+             * We shouldn't have to be the gatekeepers for quick updates or edits, but rather for publishing the changes.
+             *
+             // auth_type: 'pkce', // this is a client side auth
+             // app_id: '<string from GitHub / GitLab app config>'
+             */
             repo: 'Greenheart/idg.tools',
             branch: 'main',
-            open_authoring: false,
+            base_url: 'https://idg-cms-oauth.vercel.app',
         },
         local_backend: {
             url: 'http://localhost:2030/api/v1',
