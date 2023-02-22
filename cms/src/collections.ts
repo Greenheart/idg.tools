bd01774d67796564bb64aedae940506118524175
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-02-22 17:14:16 +0100

    Disable featured content in the CMS since it doesn't support localization yet

--- cms/src/collections.ts
+++ cms/src/collections.ts
@@ -3,13 +3,47 @@ import { STORIES_LEARN_MORE } from './constants'
 
 import { ContributorsFields } from './fields/contributors'
 import { DimensionsFields } from './fields/dimensions'
-import { FeaturedContentFields } from './fields/settings/featured'
+// import { FeaturedContentFields } from './fields/settings/featured'
 import { SkillsFields } from './fields/skills'
 import { StoriesFields } from './fields/stories'
 import { TagsFields } from './fields/tags'
 import { ToolsFields } from './fields/tools'
 
 export const COLLECTIONS = [
+    {
+        name: 'skills',
+        label: '🌱 Skills',
+        label_singular: 'Skill',
+        folder: '/content/src/skills',
+        fields: SkillsFields,
+        extension: 'json',
+        format: 'json',
+        create: false,
+        delete: false,
+        publish: false,
+        summary: '{{fields.name}}',
+        identifier_field: 'id',
+        slug: '{{id}}',
+        i18n: true,
+        description: 'The 23 skills of the Inner Development Goals.',
+    },
+    {
+        name: 'dimensions',
+        label: '🌿 Dimensions',
+        label_singular: 'Dimension',
+        folder: '/content/src/dimensions',
+        fields: DimensionsFields,
+        extension: 'json',
+        format: 'json',
+        create: false,
+        delete: false,
+        publish: false,
+        summary: '{{fields.name}}',
+        identifier_field: 'id',
+        slug: '{{id}}',
+        i18n: true,
+        description: 'The 5 dimensions of the Inner Development Goals.',
+    },
     {
         name: 'tools',
         label: '📚 Tools',
@@ -78,51 +112,19 @@ export const COLLECTIONS = [
         i18n: true,
         description: 'Create tags and use them to organize different tools.',
     },
-    {
-        name: 'settings',
-        label: '⚙️ Settings',
-        description: 'Here you can change settings like which stories that should be featured.',
-        files: [
-            {
-                label: 'Featured Content',
-                name: 'featured',
-                file: '/content/src/settings/featured.json',
-                fields: FeaturedContentFields,
-            },
-        ],
-    },
-    {
-        name: 'skills',
-        label: '🌱 Skills',
-        label_singular: 'Skill',
-        folder: '/content/src/skills',
-        fields: SkillsFields,
-        extension: 'json',
-        format: 'json',
-        create: false,
-        delete: false,
-        publish: false,
-        summary: '{{fields.name}}',
-        identifier_field: 'id',
-        slug: '{{id}}',
-        i18n: true,
-        description: 'The 23 skills of the Inner Development Goals.',
-    },
-    {
-        name: 'dimensions',
-        label: '🌿 Dimensions',
-        label_singular: 'Dimension',
-        folder: '/content/src/dimensions',
-        fields: DimensionsFields,
-        extension: 'json',
-        format: 'json',
-        create: false,
-        delete: false,
-        publish: false,
-        summary: '{{fields.name}}',
-        identifier_field: 'id',
-        slug: '{{id}}',
-        i18n: true,
-        description: 'The 5 dimensions of the Inner Development Goals.',
-    },
+    // NOTE: Since Netlify CMS doesn't support localization for file collections, we need to edit this manually for now
+    // There are experiments available for how to solve this, but it's not worth the effort at the moment.
+    // {
+    //     name: 'settings',
+    //     label: '⚙️ Settings',
+    //     description: 'Here you can change settings like which stories that should be featured.',
+    //     files: [
+    //         {
+    //             label: 'Featured Content',
+    //             name: 'featured',
+    //             file: '/content/src/settings/featured.json',
+    //             fields: FeaturedContentFields,
+    //         },
+    //     ],
+    // }
 ] as CmsCollection[]
