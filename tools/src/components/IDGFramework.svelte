1c6259754c4460dd3370ecf35f0140a1981dbc13
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-02-22 21:34:15 +0100

    Tweak styles and add outline

--- tools/src/components/IDGFramework.svelte
+++ tools/src/components/IDGFramework.svelte
@@ -3,7 +3,7 @@
     import { COLORS } from '$shared/constants'
     import { getSkillsInDimension } from '$shared/content-utils'
     import type { Dimension, Skill } from '$shared/types'
-    import { cx } from '$shared/utils'
+    import { cx, getColor } from '$shared/utils'
 
     export let skills: Skill[]
     export let dimensions: Dimension[]
@@ -21,14 +21,14 @@
         <IDGColors />
     </div>
 
-    <div class="grid aspect-video place-items-center bg-white p-4 shadow-md">
-        <div class="grid h-full w-full grid-cols-5 gap-2 p-8 text-white">
+    <div class="grid aspect-video place-items-center bg-white p-3 shadow-md">
+        <div class="grid h-full w-full grid-cols-5 gap-2 p-4 text-white">
             {#each dimensions as dimension, i}
                 {@const dimensionName = COLORS[dimension.id]}
                 <div
                     class={cx(
-                        `bg-${dimensionName}`,
-                        'grid h-full grid-rows-[max-content_94px] gap-2 p-6 shadow-sm',
+                        'grid h-full grid-rows-[max-content_100px] gap-2 p-6 shadow-sm',
+                        getColor(dimension.id),
                     )}
                 >
                     <img
@@ -42,9 +42,9 @@
                         <h2 class="font-black">
                             <span class="pr-2">{i + 1}</span>{dimension.name}
                         </h2>
-                        <p class="text-sm font-semibold">{dimension.description}</p>
+                        <p class="text-base font-semibold">{dimension.description}</p>
                     </div>
-                    <div class="flex flex-col justify-start gap-3 pt-1 text-sm font-semibold">
+                    <div class="flex flex-col justify-start gap-3 pt-1 text-base font-semibold">
                         {#each getSkillsInDimension(dimension.id, { skills }) as { name }}
                             <p>{name}</p>
                         {/each}
@@ -53,4 +53,16 @@
             {/each}
         </div>
     </div>
+
+    {#each dimensions as dimension, i}
+        {@const dimensionName = COLORS[dimension.id]}
+        <div class="grid aspect-video place-items-center bg-white p-3 shadow-md">
+            <div
+                class={cx(
+                    'grid h-full w-full grid-cols-5 gap-2 p-8 text-white',
+                    getColor(dimension.id),
+                )}
+            />
+        </div>
+    {/each}
 </div>
