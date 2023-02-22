44f2053c52734c921a565ef6f2923a19519fbbf8
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-02-22 20:49:34 +0100

    Reduce duplication

--- tools/src/components/IDGFramework.svelte
+++ tools/src/components/IDGFramework.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
     import IDGColors from '$components/IDGColors.svelte'
+    import { COLORS } from '$shared/constants'
     import { getSkillsInDimension } from '$shared/content-utils'
     import type { Dimension, Skill } from '$shared/types'
+    import { cx } from '$shared/utils'
 
     export let skills: Skill[]
     export let dimensions: Dimension[]
@@ -16,88 +18,30 @@
 
     <div class="grid aspect-video place-items-center bg-white p-4 shadow-md">
         <div class="grid h-full w-full grid-cols-5 gap-2 p-8 text-white">
-            <div class="bg-being grid h-full grid-rows-[max-content_90px] gap-2 p-6 shadow-sm">
-                <img
-                    src="/images/symbols/being.png"
-                    alt="IDG being symbol"
-                    width="150"
-                    height="150"
-                    class="place-self-center"
-                />
-                <div class="border-y py-2">
-                    <h2>{dimensions[0].name}</h2>
-                    <p class="text-sm">{dimensions[0].description}</p>
+            {#each dimensions as dimension}
+                {@const dimensionName = COLORS[dimension.id]}
+                <div
+                    class={cx(
+                        `bg-${dimensionName}`,
+                        'grid h-full grid-rows-[max-content_90px] gap-2 p-6 shadow-sm',
+                    )}
+                >
+                    <img
+                        src={`/images/symbols/${dimensionName}.png`}
+                        alt={`IDG ${dimensionName} symbol`}
+                        width="150"
+                        height="150"
+                        class="place-self-center"
+                    />
+                    <div class="border-y py-2">
+                        <h2>{dimension.name}</h2>
+                        <p class="text-sm">{dimension.description}</p>
+                    </div>
+                    {#each getSkillsInDimension(dimension.id, { skills }) as { name }}
+                        <p class="text-sm">{name}</p>
+                    {/each}
                 </div>
-                {#each getSkillsInDimension(dimensions[0].id, { skills }) as { name }}
-                    <p class="text-sm">{name}</p>
-                {/each}
-            </div>
-            <div class="bg-thinking grid h-full grid-rows-[max-content_90px] gap-2 p-6 shadow-sm">
-                <img
-                    src="/images/symbols/thinking.png"
-                    alt="IDG thinking symbol"
-                    width="150"
-                    height="150"
-                    class="place-self-center"
-                />
-                <div class="border-y py-2">
-                    <h2>{dimensions[1].name}</h2>
-                    <p class="text-sm">{dimensions[1].description}</p>
-                </div>
-                {#each getSkillsInDimension(dimensions[1].id, { skills }) as { name }}
-                    <p class="text-sm">{name}</p>
-                {/each}
-            </div>
-            <div class="bg-relating grid h-full grid-rows-[max-content_90px] gap-2 p-6 shadow-sm">
-                <img
-                    src="/images/symbols/relating.png"
-                    alt="IDG relating symbol"
-                    width="150"
-                    height="150"
-                    class="place-self-center"
-                />
-                <div class="border-y py-2">
-                    <h2>{dimensions[2].name}</h2>
-                    <p class="text-sm">{dimensions[2].description}</p>
-                </div>
-                {#each getSkillsInDimension(dimensions[2].id, { skills }) as { name }}
-                    <p class="text-sm">{name}</p>
-                {/each}
-            </div>
-            <div
-                class="bg-collaborating grid h-full grid-rows-[max-content_90px] gap-2 p-6 shadow-sm"
-            >
-                <img
-                    src="/images/symbols/collaborating.png"
-                    alt="IDG collaborating symbol"
-                    width="150"
-                    height="150"
-                    class="place-self-center"
-                />
-                <div class="border-y py-2">
-                    <h2>{dimensions[3].name}</h2>
-                    <p class="text-sm">{dimensions[3].description}</p>
-                </div>
-                {#each getSkillsInDimension(dimensions[3].id, { skills }) as { name }}
-                    <p class="text-sm">{name}</p>
-                {/each}
-            </div>
-            <div class="bg-acting grid h-full grid-rows-[max-content_90px] gap-2 p-6 shadow-sm">
-                <img
-                    src="/images/symbols/acting.png"
-                    alt="IDG acting symbol"
-                    width="150"
-                    height="150"
-                    class="place-self-center"
-                />
-                <div class="border-y py-2">
-                    <h2>{dimensions[4].name}</h2>
-                    <p class="text-sm">{dimensions[4].description}</p>
-                </div>
-                {#each getSkillsInDimension(dimensions[4].id, { skills }) as { name }}
-                    <p class="text-sm">{name}</p>
-                {/each}
-            </div>
+            {/each}
         </div>
     </div>
 </div>
