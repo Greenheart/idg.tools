67e447874202fbc6aac68070257133bd5add89ce
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-02-23 05:00:27 +0100

    Temporarily disable prerendering for the framework route

--- tools/src/routes/[[locale]]/framework/+page.server.ts
+++ tools/src/routes/[[locale]]/framework/+page.server.ts
@@ -2,6 +2,11 @@ import { error } from '@sveltejs/kit'
 import { getContent } from '$lib/content-backend'
 import type { PageServerLoad } from './$types'
 
+// TODO: Figure out a way to prerender the localized routes.
+// We have all content so we should be able to solve this if we either render all available locales and link to the content
+// or if we provide the routes that should be prerendered.
+export const prerender = false
+
 export const load = (async ({ params }) => {
     // TODO: Get the user's preferred content locale via the `Accept-Language` HTTP header
     // Build this into getLocale() so it happens automatically
