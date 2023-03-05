ae7eb9d3d97f2a9e45d6b21b622d5ca6f76172d2
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-03-05 15:51:26 +0100

    Add idea

--- community/src/routes/+layout.svelte
+++ community/src/routes/+layout.svelte
@@ -10,6 +10,7 @@
 
     // Workaround to allow smooth scrolling in Firefox
     // https://github.com/sveltejs/kit/issues/2733#issuecomment-1050779671
+    // TODO: Check if SvelteKit snapshots can solve this: https://github.com/sveltejs/kit/pull/8723#issuecomment-1423522635
     beforeNavigate(({ to, from }) => {
         if (to?.url?.pathname !== from?.url?.pathname) {
             document.documentElement.style.scrollBehavior = 'auto'
