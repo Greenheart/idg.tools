6bd172480feb1dd78ec39536b66cb7e47ac5256b
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-03-08 18:52:30 +0100

    Keep consistent layout in different states

--- tools/src/components/VisibleToolsCount.svelte
+++ tools/src/components/VisibleToolsCount.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
     import type { Tool } from '$shared/types'
-    import { visibleItems, selectedSkills, selectedTags } from '$lib/stores'
+    import { visibleItems } from '$lib/stores'
 
     export let mostRelevantTools: Tool[]
     export let allToolsCount: number
 </script>
 
-Showing {Math.min($visibleItems, mostRelevantTools.length)} of {allToolsCount} tools{#if $selectedSkills.length || $selectedTags.length}&nbsp;matching
-    your search{/if}
+Showing {Math.min($visibleItems, mostRelevantTools.length)} of {allToolsCount} tools
