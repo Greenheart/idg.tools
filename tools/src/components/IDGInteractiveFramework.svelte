8cbb3e16e99bc8ec42e063b8a65da06f78875b66
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-08-23 18:58:36 +0200

    WIP: interactive framework

---
+++ tools/src/components/IDGInteractiveFramework.svelte
@@ -0,0 +1,151 @@
+<script lang="ts">
+    import {
+        hierarchy,
+        interpolateZoom,
+        pack,
+        transition,
+        type HierarchyCircularNode,
+        type ZoomView,
+    } from 'd3'
+
+    export let data: unknown
+
+    const width = 800 //the outer width of the chart, in pixels
+    const height = width // the outer height of the chart, in pixels
+    const margin = 20 //the overall margin between the circle packs to the viewport edge
+
+    const packFunc = (packData: any) =>
+        pack()
+            .size([width - margin, height - margin])
+            .padding(3)(
+            hierarchy(packData)
+                .sum((d) => d.value)
+                .sort((a, b) => (b?.value ?? 0) - (a?.value ?? 0)),
+        )
+
+    const root = packFunc(data)
+
+    /**
+     * Expose the activeFocus to let other components react when this value changes
+     * Learn more: https://svelte.dev/docs/component-directives#bind-property
+     */
+    export let activeFocus = root
+
+    let view: ZoomView
+    let activeZoomK = (width / root.r) * 2
+    let activeZoomA = root.x
+    let activeZoomB = root.y
+
+    const inactiveZoomTo = (v: ZoomView) => {
+        activeZoomK = width / v[2]
+        view = v
+        activeZoomA = v[0]
+        activeZoomB = v[1]
+    }
+
+    inactiveZoomTo([root.x, root.y, root.r * 2 + margin])
+
+    const zoom = (d: HierarchyCircularNode<unknown>, e: Event) => {
+        e.stopPropagation()
+
+        activeFocus = d
+
+        transition()
+            .duration(600)
+            .tween('zoom', () => {
+                var i = interpolateZoom(view, [
+                    activeFocus.x,
+                    activeFocus.y,
+                    activeFocus.r * 2 + margin,
+                ])
+                return function (t) {
+                    inactiveZoomTo(i(t))
+                }
+            })
+    }
+
+    const IDG_COLORS = {
+        being: '#d4b88c',
+        thinking: '#e585a1',
+        relating: '#e84139',
+        collaborating: '#ff6821',
+        acting: '#661a30',
+    }
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-static-element-interactions -->
+<svg {width} {height} on:click={(e) => zoom(root, e)}>
+    <g transform="translate({width / 2},{height / 2})">
+        <!-- IDEA: Maybe making the skills selectable too would be a good idea, since that would allow opening details with more information -->
+        {#each root.descendants().slice(1) as rootData}
+            <!-- svelte-ignore a11y-mouse-events-have-key-events -->
+            <circle
+                class={rootData.parent
+                    ? rootData.children
+                        ? 'node'
+                        : 'node node--leaf'
+                    : 'node node--root'}
+                fill={rootData.children
+                    ? IDG_COLORS[rootData.data.name?.toLowerCase()] ?? '#f5f5f5'
+                    : '#f5f5f5'}
+                on:click={(e) => {
+                    if (activeFocus !== rootData) zoom(rootData, e)
+                }}
+                transform="translate({(rootData.x - activeZoomA) * activeZoomK},{(rootData.y -
+                    activeZoomB) *
+                    activeZoomK})"
+                r={rootData.r * activeZoomK}
+            ></circle>
+        {/each}
+        <!--
+            This always renders all the descendants of the root.
+            It would be useful to know it was showing a skill or a dimension
+        -->
+        {#each root.descendants() as rootDes}
+            <text
+                class="label"
+                style="fill-opacity: {rootDes.parent === activeFocus
+                    ? 1
+                    : 0}; display: {rootDes.parent === activeFocus ? 'inline' : 'none'};"
+                transform="translate({(rootDes.x - activeZoomA) * activeZoomK},{(rootDes.y -
+                    activeZoomB) *
+                    activeZoomK})">{rootDes.data.name}</text
+            >
+
+            <!-- TODO: Add text wrapping for labels longer than a max length -->
+            <!-- TODO: Add background and padding behind the text labels -->
+        {/each}
+    </g>
+</svg>
+
+<style>
+    .node {
+        cursor: pointer;
+    }
+
+    .node:hover {
+        stroke: #000;
+        stroke-width: 1.5px;
+    }
+
+    .node--leaf {
+        fill: white;
+    }
+
+    .label {
+        font-size: 15px;
+        font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        text-anchor: middle;
+        text-shadow:
+            0 1px 0 #fff,
+            1px 0 0 #fff,
+            -1px 0 0 #fff,
+            0 -1px 0 #fff;
+    }
+
+    /* .label, */
+    .node--root {
+        pointer-events: none;
+        user-select: none;
+    }
+</style>
--- tools/src/routes/[[locale]]/framework/+page.svelte
+++ tools/src/routes/[[locale]]/framework/+page.svelte
@@ -1,21 +1,31 @@
 <script lang="ts">
     import { Divider, Heading, Link } from '$shared/components'
 
-    import { page } from '$app/stores'
+    // import { page } from '$app/stores'
     import Meta from '$components/Meta.svelte'
-    import IDGFramework from '$components/IDGFramework.svelte'
+    // import IDGFramework from '$components/IDGFramework.svelte'
     import type { PageData } from './$types'
     import { IDG_REPORT_PDF } from '$shared/constants'
+    import IDGInteractiveFramework from '$components/IDGInteractiveFramework.svelte'
+    import { getIDGFrameworkData } from '$lib/getIDGFrameworkData'
+    import type { HierarchyCircularNode } from 'd3'
+    import { getDimension, getSkill } from '$shared/content-utils'
 
-    const url = $page.url.toString()
+    // TODO: find a way to use the page url without the store, since that causes some error
+    // const url = $page.url.toString()
 
     export let data: PageData
     $: ({ skills, dimensions } = data)
+
+    let test: HierarchyCircularNode<unknown>
+
+    $: selectedSkill = test?.data?.id ? getSkill(test?.data?.id, { skills }) : null
+    $: selectedDimension = test?.data?.id ? getDimension(test?.data?.id, { dimensions }) : null
 </script>
 
-<Meta title="IDG Framework" description="The 5 dimensions with the 23 skills and qualities" {url} />
+<Meta title="IDG Framework" description="The 5 dimensions with the 23 skills and qualities" />
 
-<Heading size={1} class="text-collaborating">IDG Framework</Heading>
+<!-- <Heading size={1} class="text-collaborating">IDG Framework</Heading>
 
 <div class="py-12 text-lg">
     <p>
@@ -25,8 +35,33 @@
             >“Inner Development Goals: Background, method and the IDG framework”</Link
         > to learn more.
     </p>
-</div>
+</div> -->
 
-<IDGFramework {skills} {dimensions} />
+<!-- <IDGFramework {skills} {dimensions} /> -->
+
+<div class="grid grid-cols-[1fr_400px]">
+    <IDGInteractiveFramework
+        data={getIDGFrameworkData(dimensions, skills)}
+        bind:activeFocus={test}
+    />
+    <div class="bg-green-400 p-4">
+        <Heading size={2} class="mt-4 mb-4"
+            >{selectedSkill?.name || selectedDimension?.name || 'Inner Development Goals'}</Heading
+        >
+        {#if selectedSkill}
+            <p>{selectedSkill.description}</p>
+        {:else if selectedDimension}
+            <p>{selectedDimension.description}</p>
+        {:else}
+            <p>
+                The 5 dimensions with the 23 skills and qualities. Read the full report <Link
+                    href={IDG_REPORT_PDF}
+                    variant="black"
+                    >“Inner Development Goals: Background, method and the IDG framework”</Link
+                > to learn more.
+            </p>
+        {/if}
+    </div>
+</div>
 
 <Divider class="my-16" />
