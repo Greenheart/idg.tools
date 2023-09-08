d15735a32ae00873bca1c9ca4e3473dad2f5eae8
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-09-08 11:06:34 +0200

    Add section

--- community/src/routes/survey/background/+page.svelte
+++ community/src/routes/survey/background/+page.svelte
@@ -40,6 +40,19 @@
         multiplier3: 'Engage in Delphi prioritisation process',
         multiplier4: 'Present the updated framework at the IDG Summit in October 2024',
         note: 'Scroll the table to see all details',
+        howDoneTitle: 'How is it done?',
+        phaseTitle1: 'Phase 1: Setting up the system (March 1 – September 2023)',
+        phaseTitle2: 'Phase 2: Data collection (September 2023  –  January 2024)',
+        phaseTitle3: 'Phase 3: Analysis & Prioritization (January  –  June 2024)',
+        phaseTitle4: 'Phase 4: Finalise design, present and localize (July  –  December 2024)',
+        md_phaseText1:
+            'During this phase co-creation have been conducted with more than 100 researchers across the globe to develop the survey and other methods. In parallel, information meetings have been conducted with so called multipliers (i.e international networks, global organizations, IDG Partner organizations & IDG Hubs) who are interested to help share the survey to reach. To get a comprehensive and inclusive dataset we aim at engaging at least 100 countries with 2M people who view the survey out which 100.000 respond. Researchers and multipliers have quality controlled the automatically generated translations of the survey.',
+        md_phaseText2:
+            'Data collection starts with the global launch, September 19. The survey will be open for gathering responses during 4 months. Reach into countries will be tracked regularly and fed back to researchers and multipliers to assure that data gathering becomes as broad and inclusive as possible. Multipliers will continually distribute the survey through social media, conferences, emails and other suitable distribution channels.',
+        md_phaseText3:
+            'Each country research team focuses on the material relevant for their country in one or several relevant languages. The process includes 4 steps:\n\n1. identify all relevant data to answer the One big question,  i.e. the individual and collective transformational skills (including qualities, abilities and skills) relevant to build a sustainable future for people and planet\n\n2. apply thematic analysis systematically to generate a list of transformational skills under each of the five dimensions being, thinking, relating, collaborating and acting\n\n3. co-leads from each country team collaborate to develop a joint comprehensive list of transformational skills and descriptions that can represent the width of concepts across all countries. The skills of the first IDG framework will be taken into account when this list is created to not loose consistency.\n\n4. This joint list will be used in a so called Delphi process to collect feedback on key prioritized transformational skills by all research team members and other respondents that have volunteered to be reached again for later stages of the survey process.\n\nIn addition to this process, qualitative data from groups that may not be able to respond to the survey and/or can be under-represented such as indigenous groups, migrants and people with low ability to read/write will be added to the material during different steps of the process.',
+        md_phaseText4:
+            'The process will result in a shared global list of transformational skills under the five dimensions of the IDG framework. This final list lay the ground for the Global IDG framework.  The global framework will be designed in collaboration between researchers, key communication specialists for the first IDG framework as well as users of the framework from policy and practice. The framework will be presented at the IDG Summit in October 2024. The global shared framework will be considered the formal framework as the shared concepts and symbols are critical to increase understanding globally.\n\nAfter the shared global framework is presented, each country/language team will start to relocalize the global shared framework to their specific context by revising the descriptions of transformational skills in alignment with what their local list (derived in earlier stages) suggest is necessary to make IDGs relevant in their context. Contextual appropriateness is an important quality to make IDGs work in each context.',
     }
 </script>
 
@@ -168,6 +181,18 @@
             </tr>
         </tbody>
     </table>
+
+    <IDGDivider />
+
+    <Heading size={1} tag="h2" class="mb-4 text-collaborating">{t.howDoneTitle}</Heading>
+    <Heading size={3} class="mb-4 mt-8">{t.phaseTitle1}</Heading>
+    <Markdown formatting="limited" source={t.md_phaseText1} />
+    <Heading size={3} class="mb-4 mt-8">{t.phaseTitle2}</Heading>
+    <Markdown formatting="limited" source={t.md_phaseText2} />
+    <Heading size={3} class="mb-4 mt-8">{t.phaseTitle3}</Heading>
+    <Markdown formatting="limited" source={t.md_phaseText3} />
+    <Heading size={3} class="mb-4 mt-8">{t.phaseTitle4}</Heading>
+    <Markdown formatting="limited" source={t.md_phaseText4} />
 </div>
 
 <style lang="postcss">
