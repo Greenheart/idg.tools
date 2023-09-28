c0ad032e893e103c411b6e307fa341b304d7bee6
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-09-28 16:38:25 +0200

    Copy questions to existing locales

--- cms/src/fields/skills.ts
+++ cms/src/fields/skills.ts
@@ -5,6 +5,7 @@ export const SkillsFields: CustomCmsField[] = [
     NAME,
     { ...DESCRIPTION, widget: 'text' } as CustomCmsField,
     {
+        // TODO: create script to populate questions for skills in other locales
         label: 'Reflection questions',
         label_singular: 'Reflection question',
         name: 'questions',
