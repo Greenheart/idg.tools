4b15abfe5c301d35e44a166427a18ac4265c6c7d
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-09-28 12:48:35 +0200

    Fix type error

--- community/src/hooks.server.ts
+++ community/src/hooks.server.ts
@@ -6,7 +6,7 @@ export const handle = (async ({ event, resolve }) => {
         transformPageChunk: ({ html, done }) => {
             const locale = getLocale(event.params.locale)
             if (done) {
-                const updated = html.replace(
+                const updated: string = html.replace(
                     '<html lang="en"',
                     `<html lang="${locale}" dir="${getHTMLDirection(locale)}"`,
                 )
