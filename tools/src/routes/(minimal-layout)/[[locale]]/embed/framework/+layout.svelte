db4a27e940ae2a88584a3d4036be1e54406e6703
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2023-10-10 22:02:52 +0200

    Experiment with iframe resizer

--- tools/src/routes/(minimal-layout)/[[locale]]/embed/framework/+layout.svelte
+++ tools/src/routes/(minimal-layout)/[[locale]]/embed/framework/+layout.svelte
@@ -1,5 +1,20 @@
-<!-- TODO: Add the iframe resize script here -->
 <!-- TODO: Add the iframe resize script to the embedding site too -->
 <!-- TODO: configure the embedding site correctly -->
+
+<script lang="ts">
+    import { IDG_WEBSITE } from '$shared/constants'
+    import 'iframe-resizer/js/iframeResizer.contentWindow'
+</script>
+
+<svelte:head>
+    <script>
+        // Since we can't add multiple targetOrigin, we might just as well disable it
+        // I guess we could allow only a few origins on the server side, and respond with 403 otherwise.
+        window.iFrameResizer = {
+            targetOrigin: IDG_WEBSITE,
+        }
+    </script>
+    <script src="/js/iframeResizer.contentWindow.min.js"></script>
+</svelte:head>
 
 <slot />
