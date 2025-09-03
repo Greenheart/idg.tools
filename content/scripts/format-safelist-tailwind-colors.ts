6f0fa91822622f211d24312329cc8011d491d9cc
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-04 01:07:12 +0200

    Quote classes to make tailwind config work

--- content/scripts/format-safelist-tailwind-colors.ts
+++ content/scripts/format-safelist-tailwind-colors.ts
@@ -19,13 +19,13 @@ console.log('\nNOTE: Add these safelist classes to the Tailwind config:\n')
 console.log(
     '\nCommunity => ./community/src/app.css\n\n' +
         getCommunitySafelist()
-            .map((c) => `@source inline(${c});`)
+            .map((c) => `@source inline('${c}');`)
             .join('\n'),
 )
 console.log(
     '\n\n\nTools => ./tools/src/app.css\n\n' +
         getToolsSafelist()
-            .map((c) => `@source inline(${c});`)
+            .map((c) => `@source inline('${c}');`)
             .join('\n') +
         '\n',
 )
