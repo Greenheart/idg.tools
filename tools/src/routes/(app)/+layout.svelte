33149517d07a2ce71b75c61ca0936017f4267077
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-11 16:26:59 +0200

    Update footer

--- tools/src/routes/(app)/+layout.svelte
+++ tools/src/routes/(app)/+layout.svelte
@@ -2,9 +2,8 @@
     import { type Snippet } from 'svelte'
 
     import Header from '$components/Header.svelte'
-    import CommunitySection from '$components/CommunitySection.svelte'
     import { Link } from '$shared/components'
-    import { GITHUB_LINK } from '$shared/constants'
+    import { GITHUB_LINK, IDG_WEBSITE } from '$shared/constants'
     interface Props {
         children?: Snippet
     }
@@ -15,14 +14,17 @@
 <main class="mx-auto w-full max-w-2xl px-4 pb-16 text-black sm:max-w-6xl sm:px-8 sm:text-lg">
     <Header />
     {@render children?.()}
-    <div class="mx-auto mt-16 max-w-2xl sm:max-w-3xl">
-        <CommunitySection />
-        <p class="mt-16 text-center">
+    <div class="mx-auto mt-16 max-w-2xl text-center sm:max-w-3xl">
+        <p>
+            Learn more about the <Link href={IDG_WEBSITE} variant="orange"
+                >Inner Development Goals</Link
+            >.
+        </p>
+
+        <p class="mt-16">
             The IDG Toolkit uses two licenses: CC-BY-SA-4.0 for content (except images), and
-            AGPL-3.0 for software.
-            <Link href={GITHUB_LINK} variant="orange" class="whitespace-nowrap"
-                >Learn more on GitHub</Link
-            >
+            AGPL-3.0 for software. Learn more on
+            <Link href={GITHUB_LINK} variant="inverted" class="whitespace-nowrap">GitHub</Link>.
         </p>
     </div>
 </main>
