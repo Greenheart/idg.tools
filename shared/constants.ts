af5b75f1556f2278fc305519037faa2728badada
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-14 13:42:24 +0200

    Update constants

--- shared/constants.ts
+++ shared/constants.ts
@@ -106,45 +106,6 @@ export const DEFAULT_LOCALE_IDENTIFIER: Locale = 'en'
  * This object should include all skills and dimensions.
  */
 export const COLORS: Record<Dimension['id'] | Skill['id'], keyof typeof IDG_COLORS_RGB> = {
-    ckzi3855r0086e2n0smi0mew5: 'being',
-    ckzi3a4z90431e2n04kx2pbq2: 'being',
-    ckzi3am4y0486e2n04obcy6hi: 'being',
-    ckzi3ayvo0547e2n03lvu0goo: 'being',
-    ckzi3b8sz0614e2n0ijnnazce: 'being',
-    ckzi3blfz0687e2n0huf88a8y: 'being',
-    ckzi38ilp0149e2n0idgw5zqv: 'thinking',
-    ckzi3cikk0766e2n0j9my4e46: 'thinking',
-    ckzi3cvpf0851e2n00bcmzsgz: 'thinking',
-    ckzi3d9gp0942e2n0lvc6k1nh: 'thinking',
-    ckzi3dk3h1039e2n0qxij7ooh: 'thinking',
-    ckzi3dtdk1142e2n0m9v02uf5: 'thinking',
-    ckzi38wix0218e2n09ncgnmxx: 'relating',
-    ckzi3e4as1251e2n0fz614xn4: 'relating',
-    ckzi3ef9s1366e2n082dhihsm: 'relating',
-    ckzi3eor91487e2n0qdoa639u: 'relating',
-    ckzi3ezut1614e2n07fab18pt: 'relating',
-    ckzi3951t0293e2n0n3bykbg5: 'collaborating',
-    ckzi3f9cj1747e2n0taoxah1j: 'collaborating',
-    ckzi3fnea2022e2n0r588if1r: 'collaborating',
-    ckzi3g17e2167e2n055jpiaaz: 'collaborating',
-    ckzi3gb7f2318e2n04kesx118: 'collaborating',
-    ckzi3gknp2475e2n0dm59ikcn: 'collaborating',
-    ckzi39fwv0374e2n0d2eynda4: 'acting',
-    ckzi3gu6u2638e2n0fmr9gv86: 'acting',
-    ckzi3h80v2807e2n0l44d20qr: 'acting',
-    ckzi3hi072982e2n0r4cdkqjy: 'acting',
-    ckzi3hqs63163e2n03h36pb73: 'acting',
-}
-
-export const IDG_COLORS_RGB = {
-    being: '#d4b88c',
-    thinking: '#e585a1',
-    relating: '#e84139',
-    collaborating: '#ff6821',
-    acting: '#661a30',
-}
-
-export const NEW_COLORS = {
     da0: 'being',
     sa0: 'being',
     sa1: 'being',
@@ -175,36 +136,10 @@ export const NEW_COLORS = {
     sc2: 'acting',
 }
 
-export const NEW_DIMENSION_IDS = {
-    ckzi3855r0086e2n0smi0mew5: 'da0',
-    ckzi38ilp0149e2n0idgw5zqv: 'da1',
-    ckzi38wix0218e2n09ncgnmxx: 'da2',
-    ckzi3951t0293e2n0n3bykbg5: 'da3',
-    ckzi39fwv0374e2n0d2eynda4: 'da4',
-}
-
-export const NEW_SKILL_IDS = {
-    ckzi3a4z90431e2n04kx2pbq2: 'sa0',
-    ckzi3am4y0486e2n04obcy6hi: 'sa1',
-    ckzi3ayvo0547e2n03lvu0goo: 'sa2',
-    ckzi3b8sz0614e2n0ijnnazce: 'sa3',
-    ckzi3blfz0687e2n0huf88a8y: 'sa4',
-    ckzi3cikk0766e2n0j9my4e46: 'sa5',
-    ckzi3cvpf0851e2n00bcmzsgz: 'sa6',
-    ckzi3d9gp0942e2n0lvc6k1nh: 'sa7',
-    ckzi3dk3h1039e2n0qxij7ooh: 'sa8',
-    ckzi3dtdk1142e2n0m9v02uf5: 'sa9',
-    ckzi3e4as1251e2n0fz614xn4: 'sb0',
-    ckzi3ef9s1366e2n082dhihsm: 'sb1',
-    ckzi3eor91487e2n0qdoa639u: 'sb2',
-    ckzi3ezut1614e2n07fab18pt: 'sb3',
-    ckzi3f9cj1747e2n0taoxah1j: 'sb4',
-    ckzi3fnea2022e2n0r588if1r: 'sb5',
-    ckzi3g17e2167e2n055jpiaaz: 'sb6',
-    ckzi3gb7f2318e2n04kesx118: 'sb7',
-    ckzi3gknp2475e2n0dm59ikcn: 'sb8',
-    ckzi3gu6u2638e2n0fmr9gv86: 'sb9',
-    ckzi3h80v2807e2n0l44d20qr: 'sc0',
-    ckzi3hi072982e2n0r4cdkqjy: 'sc1',
-    ckzi3hqs63163e2n03h36pb73: 'sc2',
+export const IDG_COLORS_RGB = {
+    being: '#d4b88c',
+    thinking: '#e585a1',
+    relating: '#e84139',
+    collaborating: '#ff6821',
+    acting: '#661a30',
 }
