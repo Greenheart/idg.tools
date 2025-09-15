0f1b19e316409a152604068a7fa6445a405a6f54
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-15 12:07:06 +0200

    Further simplify state

--- innerdevelopmentgoals/src/lib/idg-framework.svelte.ts
+++ innerdevelopmentgoals/src/lib/idg-framework.svelte.ts
@@ -12,7 +12,7 @@ export class IDGFrameworkState {
     skills: Skill[]
     readonly supportedLocales: Record<Locale, string>
 
-    #selectedDimensionId: Dimension['id']
+    selectedDimensionId: Dimension['id']
     selectedDimension: Dimension
 
     constructor(initalLocale: Locale, content: Record<Locale, WidgetContent>) {
@@ -23,21 +23,10 @@ export class IDGFrameworkState {
         this.skills = $derived(content[this.#locale].skills)
 
         // By using state for the selected dimension, this can be preserved even as the locale changes
-        this.#selectedDimensionId = $state(this.dimensions[0].id)
-
+        this.selectedDimensionId = $state(this.dimensions[0].id)
         this.selectedDimension = $derived(
-            this.dimensions.find(({ id }) => id === this.#selectedDimensionId) ??
-                this.dimensions[0],
+            this.dimensions.find(({ id }) => id === this.selectedDimensionId) ?? this.dimensions[0],
         )
-    }
-
-    // NOTE: This might be possible to replace with regular state without any special logic
-    get selectedDimensionId() {
-        return this.#selectedDimensionId
-    }
-
-    set selectedDimensionId(newDimensionId: Dimension['id']) {
-        this.#selectedDimensionId = newDimensionId
     }
 
     get locale() {
@@ -50,10 +39,10 @@ export class IDGFrameworkState {
         // Before updating the locale, we need to check if the selected dimension exist in the new locale
         // This allows us to keep the same dimension selected if it exists in the new locale.
         const newDimension =
-            dimensions.find(({ id }) => id === this.#selectedDimensionId) ?? dimensions[0]
+            dimensions.find(({ id }) => id === this.selectedDimensionId) ?? dimensions[0]
 
         this.#locale = newLocale
-        this.#selectedDimensionId = newDimension.id
+        this.selectedDimensionId = newDimension.id
     }
 }
 
