6d84cc67bb28aec39a335b76233beefaca322f70
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-15 12:03:41 +0200

    Begin simplification

--- innerdevelopmentgoals/src/lib/idg-framework.svelte.ts
+++ innerdevelopmentgoals/src/lib/idg-framework.svelte.ts
@@ -13,9 +13,7 @@ export class IDGFrameworkState {
     readonly supportedLocales: Record<Locale, string>
 
     #selectedDimensionId: Dimension['id']
-    selectedSkillId: Skill['id']
     selectedDimension: Dimension
-    selectedSkill: Skill
 
     constructor(initalLocale: Locale, content: Record<Locale, WidgetContent>) {
         this.#content = content
@@ -24,30 +22,22 @@ export class IDGFrameworkState {
         this.dimensions = $derived(content[this.#locale].dimensions)
         this.skills = $derived(content[this.#locale].skills)
 
-        // By using state for the selected dimension and skill, they can be preserved even as the locale is changed
+        // By using state for the selected dimension, this can be preserved even as the locale changes
         this.#selectedDimensionId = $state(this.dimensions[0].id)
-        this.selectedSkillId = $state(this.skills[0].id)
 
         this.selectedDimension = $derived(
             this.dimensions.find(({ id }) => id === this.#selectedDimensionId) ??
                 this.dimensions[0],
         )
-        this.selectedSkill = $derived(
-            this.skills.find(({ id }) => id === this.selectedSkillId) ?? this.skills[0],
-        )
     }
 
+    // NOTE: This might be possible to replace with regular state without any special logic
     get selectedDimensionId() {
         return this.#selectedDimensionId
     }
 
     set selectedDimensionId(newDimensionId: Dimension['id']) {
         this.#selectedDimensionId = newDimensionId
-
-        // When changing the selected dimension, make sure the selected skill is within the new dimension
-        if (!this.selectedDimension.skills.includes(this.selectedSkillId)) {
-            this.selectedSkillId = this.selectedDimension.skills[0]
-        }
     }
 
     get locale() {
@@ -55,22 +45,15 @@ export class IDGFrameworkState {
     }
 
     set locale(newLocale: Locale) {
-        const { dimensions, skills } = this.#content[newLocale]
+        const { dimensions } = this.#content[newLocale]
 
-        // Before updating the locale, we need to check if the selected dimension and skill exist in the new locale
-        // This allows us to keep the same dimension and skill selected, and falling back to other values if necessary
-        // Try to find the same dimension in the new locale, or fall back to the first dimension
+        // Before updating the locale, we need to check if the selected dimension exist in the new locale
+        // This allows us to keep the same dimension selected if it exists in the new locale.
         const newDimension =
             dimensions.find(({ id }) => id === this.#selectedDimensionId) ?? dimensions[0]
 
-        // Try to find the same skill in the new locale, or fall back to the first skill in the new dimension
-        const newSkillId =
-            skills.find(({ id }) => id === this.selectedSkillId)?.id ?? newDimension.skills[0]
-
         this.#locale = newLocale
-
         this.#selectedDimensionId = newDimension.id
-        this.selectedSkillId = newSkillId
     }
 }
 
