bd08532fa21e470cbedf5f2941ce370498a57911
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-16 16:33:44 +0200

    Verify that SSR works as expected

--- innerdevelopmentgoals/src/routes/+page.svelte
+++ innerdevelopmentgoals/src/routes/+page.svelte
@@ -1,10 +1,5 @@
 <script lang="ts">
-    import { browser } from '$app/environment'
-
     import IDGFramework from '$lib/IDGFramework.svelte'
 </script>
 
-<!-- TODO: See if it can work even during SSR -->
-{#if browser}
-    <IDGFramework />
-{/if}
+<IDGFramework />
