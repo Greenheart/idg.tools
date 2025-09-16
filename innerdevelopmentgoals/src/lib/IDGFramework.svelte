58f60295a7a495c7c2de0df31f3c135c4132238e
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-16 14:12:40 +0200

    Use inline SVG icons

--- innerdevelopmentgoals/src/lib/IDGFramework.svelte
+++ innerdevelopmentgoals/src/lib/IDGFramework.svelte
@@ -12,7 +12,6 @@
     // TODO: Create minimal CSS to be bundled together with the components
     import LocaleSelector from './LocaleSelector.svelte'
     import { getSkillsInDimension, getDimensionSlug } from '$shared/content-utils'
-    import IDGSymbol from './icons/IDGSymbol.svelte'
     import { DEFAULT_LOCALE_IDENTIFIER } from '$shared/constants'
     import type { IDGFrameworkState } from './idg-framework.svelte'
 
@@ -25,6 +24,19 @@
         return `--color: var(--${getDimensionSlug(id)});`
     }
 </script>
+
+{#snippet IDGSymbol({ id, name }: { id: keyof IDGSymbols; name: string })}
+    <svg
+        class="idg-symbol"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 198.43 198.43"
+        aria-label={name}
+    >
+        {#each symbols[id] as path}
+            <path d={path} />
+        {/each}
+    </svg>
+{/snippet}
 
 <div class="idg-framework">
     <LocaleSelector
@@ -44,7 +56,7 @@
                     title={dimension.name}
                     style={formatColorVar(dimension.id)}
                 >
-                    <IDGSymbol symbolPaths={symbols[dimension.id]} aria-label={dimension.name} />
+                    {@render IDGSymbol(dimension)}
                     <p>{dimension.name}</p>
                 </Tabs.Trigger>
             {/each}
@@ -54,7 +66,7 @@
             {@const dimensionSkills = getSkillsInDimension(dimension.id, widgetState.skills)}
             <Tabs.Content value={dimension.id} style={formatColorVar(dimension.id)}>
                 <div class="dimension">
-                    <IDGSymbol symbolPaths={symbols[dimension.id]} aria-label={dimension.name} />
+                    {@render IDGSymbol(dimension)}
                     <h2>{dimension.name}</h2>
                     <h3>{dimension.subtitle}</h3>
                     <p>{dimension.description}</p>
@@ -62,7 +74,7 @@
 
                 <div class="skills">
                     {#each dimensionSkills as skill (skill.id)}
-                        <IDGSymbol symbolPaths={symbols[skill.id]} aria-label={skill.name} />
+                        {@render IDGSymbol(skill)}
                         <div>
                             <h2>{skill.name}</h2>
                             <p>{skill.description}</p>
