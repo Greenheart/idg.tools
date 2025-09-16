6cad7a5ea12141b61230c7ecacdfd071df652b3a
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-16 18:42:32 +0200

    Remove shared modules since they are no longer in use

--- innerdevelopmentgoals/svelte.config.js
+++ innerdevelopmentgoals/svelte.config.js
@@ -1,4 +1,3 @@
-import { resolve } from 'node:path'
 import adapter from '@sveltejs/adapter-auto'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
@@ -8,16 +7,10 @@ const config = {
 
     kit: {
         adapter: adapter(),
-        alias: {
-            $shared: resolve('../shared'),
-            '$shared/*': resolve('../shared'),
-        },
         typescript: {
             config: (config) => {
                 config.include ??= []
                 config.include.push(
-                    // Add shared code and types
-                    '../shared/**/*.ts',
                     // Make sure to include scripts in the genrerated tsconfig.json to enable type checking
                     'scripts',
                 )
