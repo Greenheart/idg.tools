46bccb3c81a88a5c84e17d46589b98a64c2d6c9b
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-16 11:31:11 +0200

    Simplify IDGSymbol since we no longer use style or class attributes

--- innerdevelopmentgoals/src/lib/icons/IDGSymbol.svelte
+++ innerdevelopmentgoals/src/lib/icons/IDGSymbol.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-    import type { ClassValue } from 'svelte/elements'
     import type { IDGSymbols } from '$shared/types'
 
     interface Props {
-        class?: ClassValue
-        style?: string
         'aria-label': string
         symbolPaths: IDGSymbols[keyof IDGSymbols]
     }
@@ -12,14 +9,7 @@
     let { symbolPaths, ...props }: Props = $props()
 </script>
 
-<!-- TODO: Ideally, we shouldn't need the class or style props for the icons. Use context-aware styles instead -->
-
-<svg
-    {...props}
-    class={['idg-symbol', props.class]}
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 198.43 198.43"
->
+<svg {...props} class="idg-symbol" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 198.43 198.43">
     {#each symbolPaths as path}
         <path d={path} />
     {/each}
