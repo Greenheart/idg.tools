057715bc20cb6cb11a4547f4f33ba1864c115920
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-20 14:18:57 +0200

    Improve library demo

--- innerdevelopmentgoals/vite.config.ts
+++ innerdevelopmentgoals/vite.config.ts
@@ -18,7 +18,8 @@ const configs = {
                     name: 'IDGFramework',
                     fileName: 'components',
                 },
-                // TODO: use the same outdir in the future
+                // TODO: use the same outdir in the future, but also keep the original files for users who want them
+                // We could use this to replace the embed widget on IDG.tools to reduce the number of incoming requests
                 outDir: 'dist-js',
             },
         }),
@@ -27,14 +28,14 @@ const configs = {
 if (process.argv) {
 }
 
-export default defineConfig(({ mode, isPreview }) => {
+export default defineConfig(async ({ mode, isPreview }) => {
     if (mode === 'library') {
         if (isPreview) {
-            // Serve the demo site to showcase the built library
-            // TODO: Update the demo directory with latest build
-            // TODO: Move index.html to a demo directory
-            // TODO: Copy the latest dist files to the demo site
-            // TODO: Gitignore the dist files in the demo directory since we don't publish the demo site.
+            // Serve the demo site to showcase the latest library build
+            const fs = await import('node:fs/promises')
+            const demoDir = resolve(import.meta.dirname, 'demo')
+            await fs.cp(resolve(import.meta.dirname, 'dist-js'), demoDir, { recursive: true })
+
             return defineConfig({})
         }
 
@@ -46,3 +47,10 @@ export default defineConfig(({ mode, isPreview }) => {
 
 // Before: 469 kB transfer
 // Local after in dev mode: 2,68 MB transferred, however that might be without compression
+// After in preview mode: 589 kB => 25% more. Likely caused by the inlined fonts, and the overhead of using Svelte + bits-ui, compild into an UMD bundle.
+
+// TODO: Allow fonts to be loaded from files instead of bundled as base64
+// Additionally, we could detect if the fonts are already supported on the page and load them conditionally
+// Or we could use two CSS imports, where the library user imports the widget CSS, and optionally fonts CSS (which loads the font files)
+// This would likely reduce the build size since we don't have to load base64 fonts that are never used.
+// See https://calendar.perfplanet.com/2018/performance-anti-patterns-base64-encoding/
