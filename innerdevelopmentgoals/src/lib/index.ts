d1b07e122f9e1855d14b5906bd09dee810ef23f9
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-20 12:52:27 +0200

    Add ideas

--- innerdevelopmentgoals/src/lib/index.ts
+++ innerdevelopmentgoals/src/lib/index.ts
@@ -1,6 +1,7 @@
 import { mount } from 'svelte'
 import Component from './IDGFramework.svelte'
 
+// IDEA: Allow passing in the initial locale via props
 export default function IDGFramework(target: HTMLElement) {
     mount(Component, { target })
 }
--- innerdevelopmentgoals/vite.demo.config.ts
+++ innerdevelopmentgoals/vite.demo.config.ts
@@ -1,3 +1,6 @@
 import { defineConfig } from 'vite'
 
+// IDEA: Use environment variables to conditionally use the right vite config.
+// Use @sveltejs/package config as default for component development
+// Use another config for the Vite library mode, both for building the library and previewing it.
 export default defineConfig({})
