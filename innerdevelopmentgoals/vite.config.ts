56f91837bbb0bfc898ba66be96491a71a16ddae4
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-20 13:52:08 +0200

    Use conditional configuration based on the environment

--- innerdevelopmentgoals/vite.config.ts
+++ innerdevelopmentgoals/vite.config.ts
@@ -1,47 +1,48 @@
-import { sveltekit } from '@sveltejs/kit/vite'
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 
-// IDEA: We might be able to use svelte-package to compile the Svelte code, and then combine it with Vite library mode to build the umd bundle
-// https://stackoverflow.com/questions/75832641/how-to-compile-svelte-3-components-into-iifes-that-can-be-used-in-vanilla-js
+/** Use different configs depending on the use-case, and only instantiate the config when needed */
+const configs = {
+    dev: async () =>
+        defineConfig({
+            plugins: [(await import('@sveltejs/kit/vite')).sveltekit()],
+        }),
+    library: async () =>
+        defineConfig({
+            plugins: [(await import('@sveltejs/vite-plugin-svelte')).svelte()],
+            // TODO: Write a blog post and better documentation for this approach to create web components with Svelte:
+            // https://stackoverflow.com/questions/75832641/how-to-compile-svelte-3-components-into-iifes-that-can-be-used-in-vanilla-js
+            build: {
+                lib: {
+                    entry: resolve(import.meta.dirname, 'dist/index.js'),
+                    name: 'IDGFramework',
+                    fileName: 'components',
+                },
+                // TODO: use the same outdir in the future
+                outDir: 'dist-js',
+            },
+        }),
+}
 
-// const baseConfig = {
-//     plugins: [],
-//     external: [/\.woff2$/],
-// }
+if (process.argv) {
+}
 
-// const entries = [
-//     {
-//         input: './src/lib/index.ts',
-//         output: './index.js',
-//     },
-//     {
-//         input: './src/lib/index.ts',
-//         output: './index.js',
-//         format: 'es',
-//     },
-// ]
+export default defineConfig(({ mode, isPreview }) => {
+    if (mode === 'library') {
+        if (isPreview) {
+            // Serve the demo site to showcase the built library
+            // TODO: Update the demo directory with latest build
+            // TODO: Move index.html to a demo directory
+            // TODO: Copy the latest dist files to the demo site
+            // TODO: Gitignore the dist files in the demo directory since we don't publish the demo site.
+            return defineConfig({})
+        }
 
-export default defineConfig({
-    plugins: [sveltekit()],
-    build: {
-        // lib: {
-        //     entry: ['./dist/index.js'],
-        //     name: 'IDGFramework',
-        //     formats: ['es'],
-        // },
-        // rollupOptions: entries.map(({ input, output, format = 'es' }) => ({
-        //     input,
-        //     output: {
-        //         format,
-        //         file: output,
-        //         exports: 'auto',
-        //     },
-        //     external: baseConfig.external,
-        //     plugins: baseConfig.plugins,
-        // })),
-        // rollupOptions: {
-        //     input: './src/lib/index.ts',
-        //     output:
-        // }
-    },
+        return configs.library()
+    }
+
+    return configs.dev()
 })
+
+// Before: 469 kB transfer
+// Local after in dev mode: 2,68 MB transferred, however that might be without compression
--- innerdevelopmentgoals/vite.demo.config.ts
+++
@@ -1,6 +0,0 @@
-import { defineConfig } from 'vite'
-
-// IDEA: Use environment variables to conditionally use the right vite config.
-// Use @sveltejs/package config as default for component development
-// Use another config for the Vite library mode, both for building the library and previewing it.
-export default defineConfig({})
