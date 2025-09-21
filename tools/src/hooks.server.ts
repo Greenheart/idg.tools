b7e7e1172d105f281c852ce60f880888ca57bc43
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-21 15:28:53 +0200

    Fix import for HTML template to make it work for both dev and prod

--- tools/src/hooks.server.ts
+++ tools/src/hooks.server.ts
@@ -2,13 +2,16 @@ import { building, dev } from '$app/environment'
 import { getHTMLDirection, getLocale } from '$shared/content-utils'
 
 import { readFile } from 'fs/promises'
-import { resolve } from 'path'
+import { resolve as resolvePath } from 'path'
 
-const embedHTML = await readFile(resolve(import.meta.dirname, 'framework.html'), 'utf-8')
 const isFrameworkEmbed = /\/embed\/framework/
 
 export const handle = async ({ event, resolve }) => {
     if ((dev || building) && isFrameworkEmbed.test(event.url.pathname)) {
+        const embedHTML = await readFile(
+            resolvePath('static/assets/widgets/framework.html'),
+            'utf-8',
+        )
         const locale = getLocale(event.params.locale)
 
         return new Response(embedHTML.replaceAll('%LOCALE%', locale), {
