5c0685c428e1f8df69736a4ae8fbb06a5678522e
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-21 15:17:41 +0200

    Implement minimal SSR for framework embed to avoid preloading of unused SvelteKit assets

--- tools/src/hooks.server.ts
+++ tools/src/hooks.server.ts
@@ -1,6 +1,21 @@
+import { building, dev } from '$app/environment'
 import { getHTMLDirection, getLocale } from '$shared/content-utils'
 
+import { readFile } from 'fs/promises'
+import { resolve } from 'path'
+
+const embedHTML = await readFile(resolve(import.meta.dirname, 'framework.html'), 'utf-8')
+const isFrameworkEmbed = /\/embed\/framework/
+
 export const handle = async ({ event, resolve }) => {
+    if ((dev || building) && isFrameworkEmbed.test(event.url.pathname)) {
+        const locale = getLocale(event.params.locale)
+
+        return new Response(embedHTML.replaceAll('%LOCALE%', locale), {
+            headers: { 'Content-Type': 'text/html' },
+        })
+    }
+
     return resolve(event, {
         transformPageChunk: ({ html, done }) => {
             if (done) {
