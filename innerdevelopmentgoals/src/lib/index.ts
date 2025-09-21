0e4a17c49608134fde300fb28736d1f36e987837
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-21 15:58:46 +0200

    Include supported locales in the library build

--- innerdevelopmentgoals/src/lib/index.ts
+++ innerdevelopmentgoals/src/lib/index.ts
@@ -1,6 +1,10 @@
 import { mount, type ComponentProps } from 'svelte'
 import Widget from './IDGFramework.svelte'
+import { locales as supportedLocales } from './content'
+import type { Locale } from './types'
 
-export default function IDGFramework(target: HTMLElement, props: ComponentProps<typeof Widget>) {
+export function IDGFramework(target: HTMLElement, props: ComponentProps<typeof Widget>) {
     mount(Widget, { target, props })
 }
+
+export const locales = Object.keys(supportedLocales) as unknown as Locale[]
