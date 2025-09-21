cbc17dc18b181fe8dac5e83acfddbd960a5ffe35
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-21 13:29:42 +0200

    Use workspace package to implement the new design for the framework embed

--- tools/src/routes/(minimal-layout)/[[locale]]/embed/framework/+page.svelte
+++ tools/src/routes/(minimal-layout)/[[locale]]/embed/framework/+page.svelte
@@ -1,38 +1,16 @@
 <script lang="ts">
-    import { page } from '$app/state'
-    import Meta from '$components/Meta.svelte'
-    import IDGFramework from '$components/IDGFramework.svelte'
-    import { parseDimensionSlug } from '$shared/content-utils'
-    import { browser } from '$app/environment'
+    import 'innerdevelopmentgoals/framework.css'
+    import IDGFramework from 'innerdevelopmentgoals/framework'
     import { bodyClass } from '$shared/utils'
+    import { onMount } from 'svelte'
 
-    let { data } = $props()
+    let container: HTMLDivElement
 
-    // Ensure the page re-renders when the locale changes.
-    const dimensions = $derived(page && data.dimensions)
-    const skills = $derived(page && data.skills)
-    const symbols = $derived(page && data.symbols)
-
-    const lockedDimension = $derived(
-        browser ? parseDimensionSlug(page.url.hash.toLowerCase().replace('#', '')) : undefined,
-    )
+    onMount(() => {
+        IDGFramework(container)
+    })
 </script>
-
-<Meta
-    title="Inner Development Goals Framework"
-    description="The {dimensions.length} dimensions with the {skills.length} skills and qualities"
-/>
 
 <svelte:body use:bodyClass={'bg-white'} />
 
-<div class="xs:px-2">
-    <IDGFramework
-        {dimensions}
-        {skills}
-        {symbols}
-        pathname={page.url.pathname}
-        {lockedDimension}
-        currentLocale={page.params.locale}
-        supportedLocales={data.supportedLocales}
-    />
-</div>
+<div class="xs:px-2" bind:this={container}></div>
