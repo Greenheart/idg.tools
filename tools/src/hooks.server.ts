7f3b0dc89a848df92aa6251463c7bbdaa404bbeb
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-21 15:02:49 +0200

    Only update locale if needed

--- tools/src/hooks.server.ts
+++ tools/src/hooks.server.ts
@@ -3,8 +3,8 @@ import { getHTMLDirection, getLocale } from '$shared/content-utils'
 export const handle = async ({ event, resolve }) => {
     return resolve(event, {
         transformPageChunk: ({ html, done }) => {
-            const locale = getLocale(event.params.locale)
             if (done) {
+                const locale = getLocale(event.params.locale)
                 const updated: string = html.replace(
                     '<html lang="en"',
                     `<html lang="${locale}" dir="${getHTMLDirection(locale)}"`,
