66c6978780111b0ef8c74ad278d5e89ff2fab39a
Samuel Plumppu <6125097+Greenheart@users.noreply.github.com>
2025-09-24 17:40:47 +0200

    No need to recreate the static assets dir since it's tracked by Git and not supposed to be cleared

--- tools/vite.config.js
+++ tools/vite.config.js
@@ -2,7 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import { setDefaultResultOrder } from 'dns'
-import { cp, mkdir } from 'fs/promises'
+import { cp } from 'fs/promises'
 import { resolve } from 'path'
 
 setDefaultResultOrder('verbatim')
@@ -14,14 +14,11 @@ export default defineConfig(async ({ command, mode }) => {
 
     if (command === 'build' || mode === 'development') {
         // Copy over the latest framework embed assets
-        const frameworkAssetsDir = resolve(import.meta.dirname, 'static/assets/widgets')
-        await mkdir(frameworkAssetsDir, { recursive: true })
-
         await Promise.all(
             ['framework.css', 'framework.umd.js'].map((file) =>
                 cp(
                     resolve(import.meta.dirname, 'node_modules/innerdevelopmentgoals/dist/', file),
-                    resolve(frameworkAssetsDir, file),
+                    resolve(resolve(import.meta.dirname, 'static/assets/widgets'), file),
                 ),
             ),
         )
